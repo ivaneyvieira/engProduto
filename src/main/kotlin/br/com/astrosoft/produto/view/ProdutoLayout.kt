f29cfb3d96c0e35ed4541dbb9c8d1f126d010647
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-12-21 21:27:41 -0300

    Ajuste para adequação do aplicativo

--- src/main/kotlin/br/com/astrosoft/produto/view/ProdutoLayout.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/ProdutoLayout.kt
@@ -6,13 +6,11 @@ import br.com.astrosoft.framework.view.MainLayout
 import com.vaadin.flow.component.dependency.JsModule
 import com.vaadin.flow.component.icon.VaadinIcon.FORM
 import com.vaadin.flow.component.icon.VaadinIcon.USER
-import com.vaadin.flow.component.page.Push
 import com.vaadin.flow.component.tabs.Tabs
 import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
 
 @Theme(value = Lumo::class, variant = Lumo.DARK)
-@Push
 @JsModule("./styles/shared-styles.js")
 class ProdutoLayout : MainLayout() {
   override fun Tabs.menuConfig() {
