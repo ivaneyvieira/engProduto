1256ccf62719034c3beb7c724b128089f56f41ce
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-02-25 17:06:58 -0300

    Atualização cosulta

--- src/main/resources/sqlSaci/findNotaEntradaBase.sql
+++ src/main/resources/sqlSaci/findNotaEntradaBase.sql
@@ -20,8 +20,8 @@ WHERE I.storeno IN (2, 3, 4, 5)
   AND date >= 20220225
   AND I.type = 0
   AND I.vendno NOT IN (SELECT no FROM sqldados.vend WHERE V.name LIKE 'ENGECOPI%')
-  AND I.bits * POW(2, 4) = 0
-  AND I.bits * POW(2, 6) = 0
+  AND I.bits & POW(2, 4) = 0
+  AND I.bits & POW(2, 6) = 0
   AND I.cfo NOT IN (1551, 2551, 1556, 2556)
   AND (I.storeno = :loja OR :loja = 0)
   AND (I.invno = :ni OR :ni = 0)
