90be263d705f1ca1f97b55522ce414c281d1756b
Ivaney Sales <ivaneyvieira@gmail.com>
2022-11-03 09:29:53 -0300

    Ajuste na consulta de produtos

--- src/main/resources/sqlSaci/findProdutosNFSaida.sql
+++ src/main/resources/sqlSaci/findProdutosNFSaida.sql
@@ -1,31 +1,43 @@
-SELECT X.storeno                                          AS loja,
-       pdvno                                              AS pdvno,
-       xano                                               AS xano,
-       CAST(CONCAT(N.nfno, '/', N.nfse) AS CHAR)          AS nota,
-       CAST(TRIM(P.no) AS CHAR)                           AS codigo,
-       IFNULL(X.grade, '')                                AS grade,
-       TRIM(IFNULL(B.barcode, P.barcode))                 AS barcode,
-       TRIM(MID(P.name, 1, 37))                           AS descricao,
-       P.mfno                                             AS vendno,
-       IFNULL(F.auxChar1, '')                             AS fornecedor,
-       P.typeno                                           AS typeno,
-       IFNULL(T.name, '')                                 AS typeName,
-       CAST(LPAD(P.clno, 6, '0') AS CHAR)                 AS clno,
-       IFNULL(cl.name, '')                                AS clname,
-       P.m6                                               AS altura,
-       P.m4                                               AS comprimento,
-       P.m5                                               AS largura,
-       P.sp / 100                                         AS precoCheio,
-       IFNULL(S.ncm, '')                                  AS ncm,
-       X.qtty / 1000                                      AS quantidade,
-       X.preco                                            AS preco,
-       (X.qtty / 1000) * X.preco                          AS total,
-       X.c6                                               AS gradeAlternativa,
-       X.s12                                              AS marca,
-       X.c5                                               AS usuarioExp,
-       CAST(MID(IFNULL(L.localizacao, ''), 1, 4) AS CHAR) AS localizacao,
-       X.c4                                               AS usuarioCD,
-       N.tipo                                             AS tipoNota
+DROP TEMPORARY TABLE IF EXISTS T_LOC;
+CREATE TEMPORARY TABLE T_LOC (
+  PRIMARY KEY (prdno, loc)
+)
+SELECT P.no                                                                     AS prdno,
+       IF(:marca = 999, '', CAST(MID(IFNULL(L.localizacao, ''), 1, 4) AS CHAR)) AS loc
+FROM sqldados.prd           AS P
+  LEFT JOIN sqldados.prdloc AS L
+	      ON P.no = L.prdno
+WHERE (MID(L.localizacao, 1, 4) IN (:locais) OR 'TODOS' IN (:locais))
+GROUP BY prdno, loc;
+
+SELECT X.storeno                                 AS loja,
+       pdvno                                     AS pdvno,
+       xano                                      AS xano,
+       CAST(CONCAT(N.nfno, '/', N.nfse) AS CHAR) AS nota,
+       CAST(TRIM(P.no) AS CHAR)                  AS codigo,
+       IFNULL(X.grade, '')                       AS grade,
+       TRIM(IFNULL(B.barcode, P.barcode))        AS barcode,
+       TRIM(MID(P.name, 1, 37))                  AS descricao,
+       P.mfno                                    AS vendno,
+       IFNULL(F.auxChar1, '')                    AS fornecedor,
+       P.typeno                                  AS typeno,
+       IFNULL(T.name, '')                        AS typeName,
+       CAST(LPAD(P.clno, 6, '0') AS CHAR)        AS clno,
+       IFNULL(cl.name, '')                       AS clname,
+       P.m6                                      AS altura,
+       P.m4                                      AS comprimento,
+       P.m5                                      AS largura,
+       P.sp / 100                                AS precoCheio,
+       IFNULL(S.ncm, '')                         AS ncm,
+       X.qtty / 1000                             AS quantidade,
+       X.preco                                   AS preco,
+       (X.qtty / 1000) * X.preco                 AS total,
+       X.c6                                      AS gradeAlternativa,
+       X.s12                                     AS marca,
+       X.c5                                      AS usuarioExp,
+       L.loc                                     AS localizacao,
+       X.c4                                      AS usuarioCD,
+       N.tipo                                    AS tipoNota
 FROM sqldados.prd             AS P
   INNER JOIN sqldados.xaprd2  AS X
 	       ON P.no = X.prdno
@@ -33,8 +45,8 @@ FROM sqldados.prd             AS P
 	       USING (storeno, pdvno, xano)
   LEFT JOIN  sqldados.prdbar  AS B
 	       ON P.no = B.prdno AND B.grade = X.grade
-  LEFT JOIN  sqldados.prdloc  AS L
-	       ON L.prdno = P.no AND L.storeno = :storeno
+  LEFT JOIN  T_LOC            AS L
+	       ON L.prdno = P.no
   LEFT JOIN  sqldados.vend    AS F
 	       ON F.no = P.mfno
   LEFT JOIN  sqldados.type    AS T
@@ -47,6 +59,5 @@ WHERE X.storeno = :storeno
   AND X.pdvno = :pdvno
   AND X.xano = :xano
   AND (X.s12 = :marca OR :marca = 999)
-  AND (MID(L.localizacao, 1, 4) IN (:locais) OR 'TODOS' IN (:locais))
 GROUP BY codigo, grade
 
