73ef37c6e18e0c0bc8038f68e37ebcfa5d2ed3be
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-11-13 15:08:27 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/model/beans/ProdutoTransfRessu4.kt
+++ src/main/kotlin/br/com/astrosoft/produto/model/beans/ProdutoTransfRessu4.kt
@@ -1,11 +1,14 @@
 package br.com.astrosoft.produto.model.beans
 
+import java.time.LocalDate
+
 class ProdutoTransfRessu4(
   var storeno: Int,
   var pdvno: Int,
   var xano: Int,
   var notaTransf: String?,
   var rota: String?,
+  var data: LocalDate?,
   var codigo: String?,
   var descricao: String?,
   var grade: String?,
--- src/main/kotlin/br/com/astrosoft/produto/model/printText/NotaTransferencia.kt
+++ src/main/kotlin/br/com/astrosoft/produto/model/printText/NotaTransferencia.kt
@@ -1,5 +1,6 @@
 package br.com.astrosoft.produto.model.printText
 
+import br.com.astrosoft.framework.util.format
 import br.com.astrosoft.framework.util.lpad
 import br.com.astrosoft.framework.viewmodel.PrintText
 import br.com.astrosoft.produto.model.beans.ProdutoTransfRessu4
@@ -7,17 +8,19 @@ import br.com.astrosoft.produto.model.beans.ProdutoTransfRessu4
 class NotaTransferencia : PrintText<ProdutoTransfRessu4>() {
   init {
     column(ProdutoTransfRessu4::codigoFormat, "Codigo", 6)
-    column(ProdutoTransfRessu4::descricao, "Descricao", 37)
-    column(ProdutoTransfRessu4::grade, "Grade", 9, lineBreak = true)
-    column(ProdutoTransfRessu4::codigoBarras, "Codigo Barras", 15)
-    column(ProdutoTransfRessu4::referencia, "Referencia", 31)
-    column(ProdutoTransfRessu4::quant, "Quant", 6)
+    column(ProdutoTransfRessu4::descricao, "Descricao", 40)
+    column(ProdutoTransfRessu4::grade, "Grade", 9)
+    column(ProdutoTransfRessu4::quant, "Quant", 6, lineBreak = true)
+    column(ProdutoTransfRessu4::codigoBarras, "Codigo Barras", 21)
+    column(ProdutoTransfRessu4::referencia, "Referencia", 42)
   }
 
   override fun titleLines(bean: ProdutoTransfRessu4): List<String> {
+    val titulo = "NF Transf ${bean.notaTransf} - ${bean.rota}"
+    val data = "Data: ${bean.data?.format() ?: "  /  /    "}".lpad(64 - titulo.length, " ")
     return listOf(
-      "NF Transf ${bean.notaTransf} - ${bean.rota}",
-      "".lpad(54, "-")
+      "$titulo$data",
+      "".lpad(64, "-")
     )
   }
 }
