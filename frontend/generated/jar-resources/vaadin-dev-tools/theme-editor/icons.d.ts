969182a990215fa43030bc4c04a2750adda253bf
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-11-15 11:19:22 -0300

    Refactor

--- frontend/generated/jar-resources/vaadin-dev-tools/theme-editor/icons.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const icons: {
-    crosshair: import("lit-html").TemplateResult<2>;
-    square: import("lit-html").TemplateResult<2>;
-    font: import("lit-html").TemplateResult<2>;
-    undo: import("lit-html").TemplateResult<2>;
-    redo: import("lit-html").TemplateResult<2>;
-    cross: import("lit-html").TemplateResult<2>;
-};
