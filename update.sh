648377f122b9e9a9edde1586d6df0ec097dec8e5
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-11-26 11:03:30 -0300

    Docker

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
+source variables.env
+
 mkdir -p cache/gradle
 mkdir -p cache/vaadin
 
 git pull
-docker-compose build
-docker-compose down
-docker-compose up -d --build
-docker cp eng_substi:/gradle cache
-docker cp eng_substi:/vaadin cache
+docker-compose --env-file variables.env build
+docker-compose --env-file variables.env down
+docker-compose --env-file variables.env up -d --build
+docker cp eng_produtoh:/gradle cache
+docker cp eng_produtoh:/vaadin cache
