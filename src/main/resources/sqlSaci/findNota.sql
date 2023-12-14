25f6623708b4e70eaae2a2afec918d742aabcc6d
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-12-14 16:58:37 -0300

    Ajuste na busca da nota do cliente

--- src/main/resources/sqlSaci/findNota.sql
+++ src/main/resources/sqlSaci/findNota.sql
@@ -1,11 +1,11 @@
-SELECT N.nfno   AS               numero,
-       N.nfse   AS               serie,
-       CAST(N.issuedate AS DATE) data,
-       N.custno AS               cliente,
-       C.name   AS               nomeCliente,
-       N.empno  AS               vendedor,
-       V.name   AS               nomeVendedor
-FROM sqldados.nf AS N
+SELECT N.nfno   AS          numero,
+       N.nfse   AS          serie,
+       CAST(N.date AS DATE) data,
+       N.custno AS          cliente,
+       C.name   AS          nomeCliente,
+       N.empno  AS          vendedor,
+       V.name   AS          nomeVendedor
+FROM sqlpdv.pxa AS N
        INNER JOIN sqldados.custp AS C
                   ON N.custno = C.no
        LEFT JOIN sqldados.emp AS V
@@ -13,5 +13,6 @@ FROM sqldados.nf AS N
 WHERE N.storeno IN (2, 3, 4, 5, 6, 8)
   AND N.nfno = :nfno
   AND N.nfse = :nfse
-ORDER BY N.issuedate DESC
+  AND N.date BETWEEN SUBDATE(:data, 60) * 1 AND ADDDATE(:data, 2) * 1
+ORDER BY N.date DESC
 LIMIT 1
