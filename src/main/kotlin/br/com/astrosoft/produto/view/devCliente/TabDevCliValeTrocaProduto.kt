9de64a2b038fe9b5799465195b186ef67c481563
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-12-19 16:08:14 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/view/devCliente/TabDevCliValeTrocaProduto.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/devCliente/TabDevCliValeTrocaProduto.kt
@@ -20,6 +20,7 @@ import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.select.Select
 import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.value.ValueChangeMode
 import java.time.LocalDate
 
 class TabDevCliValeTrocaProduto(val viewModel: TabDevCliValeTrocaProdutoViewModel) :
@@ -48,6 +49,8 @@ class TabDevCliValeTrocaProduto(val viewModel: TabDevCliValeTrocaProdutoViewMode
       }
     }
     edtPesquisa = textField("Pesquisa") {
+      this.width = "300px"
+      valueChangeMode = ValueChangeMode.TIMEOUT
       addValueChangeListener {
         viewModel.updateView()
       }
