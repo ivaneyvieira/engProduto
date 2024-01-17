567ca015578099046f4a213b262df6d4317eb225
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-01-17 10:16:31 -0300

    Atualização da consulta

--- src/main/resources/sqlSaci/entradaDevCli.sql
+++ src/main/resources/sqlSaci/entradaDevCli.sql
@@ -100,6 +100,7 @@ FROM sqldados.inv AS I
        LEFT JOIN sqldados.store AS S
                  ON S.no = I.storeno
 WHERE I.account = '2.01.25'
+  AND I.cfo NOT LIKE '%949'
   AND I.bits & POW(2, 4) = 0
   AND (I.storeno IN (1, 2, 3, 4, 5, 6, 7, 8))
   AND (I.storeno = :loja OR :loja = 0)
