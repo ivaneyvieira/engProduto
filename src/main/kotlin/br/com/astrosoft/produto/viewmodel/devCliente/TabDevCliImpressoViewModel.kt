d19806c8125d1ea35a62bac04c4ff8cb58d43ac7
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-01-23 14:54:28 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/viewmodel/devCliente/TabDevCliImpressoViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/devCliente/TabDevCliImpressoViewModel.kt
@@ -26,7 +26,7 @@ class TabDevCliImpressoViewModel(val viewModel: DevClienteViewModel) {
   }
 
   fun imprimeValeTroca(nota: EntradaDevCli) {
-    val relatorio = ValeTrocaDevolucao(nota)
+    val relatorio = ValeTrocaDevolucao(nota, nota.nameAutorizacao ?: "")
     relatorio.print(nota.produtos(), subView.printerPreview(showPrinter = AppConfig.isAdmin, loja = 0) {
       updateView()
     })
