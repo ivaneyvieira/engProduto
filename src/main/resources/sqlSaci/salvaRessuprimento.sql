1ec028361395db95dee9f4d8e45f5c830ed01fb7
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-04-02 13:02:35 -0300

    Conserta BUG

--- src/main/resources/sqlSaci/salvaRessuprimento.sql
+++ src/main/resources/sqlSaci/salvaRessuprimento.sql
@@ -1,4 +1,5 @@
+SET SQL_MODE = '';
 REPLACE sqldados.ordsAdicional(storeno, ordno, localizacao, observacao)
-VALUE(:storeno, :ordno, MID(:localizacao,1, 4), :observacao)
+  VALUE (:storeno, :ordno, MID(:localizacao, 1, 4), :observacao)
 
 
