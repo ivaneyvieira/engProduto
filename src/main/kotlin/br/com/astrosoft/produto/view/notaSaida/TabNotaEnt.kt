3fc6a02b81ff14a0767ea1d989a82de81a49f83e
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-04-08 14:36:39 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/view/notaSaida/TabNotaEnt.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/notaSaida/TabNotaEnt.kt
@@ -40,6 +40,7 @@ class TabNotaEnt(val viewModel: TabNotaEntViewModel) : TabPanelGrid<NotaSaida>(N
   private lateinit var edtDataInicial: DatePicker
   private lateinit var edtDataFinal: DatePicker
   private lateinit var edtPesquisa: TextField
+  private lateinit var cmbTipoNF: Select<String>
 
   fun init() {
     val user = AppConfig.userLogin() as? UserSaci
@@ -82,7 +83,7 @@ class TabNotaEnt(val viewModel: TabNotaEntViewModel) : TabPanelGrid<NotaSaida>(N
       }
     }
     cmbNota = select("Nota") {
-      val user =  AppConfig.userLogin() as? UserSaci
+      val user = AppConfig.userLogin() as? UserSaci
       setItems(ETipoNota.entries)
       value = ETipoNota.TODOS
       val tipoNota = ETipoNota.entries.firstOrNull { it.num == user?.tipoNota }
@@ -90,6 +91,14 @@ class TabNotaEnt(val viewModel: TabNotaEntViewModel) : TabPanelGrid<NotaSaida>(N
       this.setItemLabelGenerator {
         it.descricao
       }
+      addValueChangeListener {
+        if (it.isFromClient)
+          viewModel.updateView()
+      }
+    }
+    cmbTipoNF = select("Tipo NF") {
+      setItems("VENDA", "TRANSFERENCIA", "ENTRE FUT", "SIMP REME", "TODOS")
+      value = "VENDA"
       addValueChangeListener {
         if (it.isFromClient)
           viewModel.updateView()
@@ -130,7 +139,8 @@ class TabNotaEnt(val viewModel: TabNotaEntViewModel) : TabPanelGrid<NotaSaida>(N
       loja = cmbLoja.value?.no ?: 0,
       dataInicial = edtDataInicial.value,
       dataFinal = edtDataFinal.value,
-      pesquisa = edtPesquisa.value ?: ""
+      pesquisa = edtPesquisa.value ?: "",
+      tipoNF = cmbTipoNF.value ?: "VENDA",
     )
   }
 
