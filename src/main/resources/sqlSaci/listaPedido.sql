6ed605ac9233b08d2e53fba0eb6c210f4b578d15
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-04-15 14:49:56 -0300

    Adição das notas de retira Futura no painel retira

--- src/main/resources/sqlSaci/listaPedido.sql
+++ src/main/resources/sqlSaci/listaPedido.sql
@@ -1,4 +1,5 @@
 USE sqldados;
+
 SET SQL_MODE = '';
 
 DO @PESQUISA := :pesquisa;
@@ -79,6 +80,148 @@ WHERE (E.storeno IN (4))
   AND (E.empno = 440)
   AND P.date >= @DATA
 GROUP BY E.storeno, E.ordno;
+
+DROP TEMPORARY TABLE IF EXISTS T_CARGA;
+CREATE TEMPORARY TABLE T_CARGA
+(
+  PRIMARY KEY (storeno, pdvno, xano),
+  INDEX (storeno, ordno)
+)
+SELECT P.storeno,
+       P.pdvno,
+       P.xano,
+       N.auxLong1  AS ordno,
+       N.time,
+       E.other     AS fre_amt,
+       N.date      AS data_venda,
+       N.nfno      AS nfno_venda,
+       N.nfse      AS nfse_venda,
+       NF.grossamt AS valor_venda,
+       N.date      AS data_entrega,
+       N.nfno      AS nfno_entrega,
+       N.nfse      AS nfse_entrega,
+       NF.grossamt AS valor_entrega
+FROM sqldados.nfrprd AS P
+       INNER JOIN sqldados.nfr AS N
+                  USING (storeno, pdvno, xano)
+       INNER JOIN sqldados.nf AS NF
+                  USING (storeno, pdvno, xano)
+       LEFT JOIN sqldados.eord AS E
+                 ON E.storeno = N.storeno
+                   AND E.ordno = N.auxLong1
+WHERE (P.storeno != :storeno OR :storeno = 0)
+  AND P.storeno != P.storenoStk
+  AND N.date > 20240401
+  AND P.optionEntrega % 10 = 4
+  AND N.nfse != 3
+GROUP BY storeno, pdvno, xano;
+
+DROP TEMPORARY TABLE IF EXISTS VENDA_CARGA;
+CREATE TEMPORARY TABLE VENDA_CARGA
+SELECT EO.storeno                                                             AS loja,
+       S.name                                                                 AS nomeLoja,
+       S.sname                                                                AS siglaLoja,
+       EO.ordno                                                               AS pedido,
+       MID(EO.c3, 1, 1)                                                       AS marca,
+       MID(EO.c3, 3, 1)                                                       AS separado,
+       MID(EO.c3, 4, 1)                                                       AS zonaCarga,
+       MID(EO.c3, 5, 8) * 1                                                   AS entrega,
+       CAST(P.date AS DATE)                                                   AS data,
+       CAST(IF(EO.dataEntrega = 0, NULL, EO.dataEntrega) AS DATE)             AS dataEntrega,
+       EO.pdvno                                                               AS pdvno,
+       SEC_TO_TIME(P.time)                                                    AS hora,
+
+       T2.pdvno                                                               AS pdvnoVenda,
+
+       IFNULL(CAST(T2.nfno_venda AS CHAR), '')                                AS nfnoFat,
+       IFNULL(T2.nfse_venda, '')                                              AS nfseFat,
+       IF(T2.data_venda = 0, NULL, CAST(T2.data_venda AS DATE))               AS dataFat,
+       SEC_TO_TIME(nff2.auxLong4)                                             AS horaFat,
+       (valor_venda / 100)                                                    AS valorFat,
+
+       IFNULL(CAST(T2.nfno_entrega AS CHAR), '')                              AS nfnoEnt,
+       IFNULL(T2.nfse_entrega, '')                                            AS nfseEnt,
+       IF(T2.data_entrega = 0, NULL, CAST(T2.data_entrega AS DATE))           AS dataEnt,
+       SEC_TO_TIME(nfe2.auxLong4)                                             AS horaEnt,
+       (valor_entrega / 100)                                                  AS valorEnt,
+
+       IFNULL(E.no, 0)                                                        AS vendno,
+       CAST(CONCAT(E.no, '-', E.name) AS CHAR)                                AS vendedor,
+       IFNULL(C.no, 0)                                                        AS custno,
+       CAST(CONCAT(LPAD(C.no * 1, 6, '0'), '-', C.name) AS CHAR)              AS cliente,
+       CAST(CONCAT('(', IFNULL(CA.ddd, LEFT(C.ddd, 3)), ')',
+                   IFNULL(CA.tel, LEFT(C.tel, 10))) AS CHAR)                  AS foneCliente,
+       CAST(RPAD(IFNULL(CA.city, C.city1), 20, ' ') AS CHAR)                  AS cidade,
+       CAST(IFNULL(CA.state, C.state1) AS CHAR)                               AS estado,
+       C.add1                                                                 AS endereco,
+       C.nei1                                                                 AS bairro,
+       CAST(RPAD(IFNULL(CA.addr, C.add1), 60, ' ') AS CHAR)                   AS enderecoEntrega,
+       RPAD(IFNULL(CA.nei, C.nei1), 25, ' ')                                  AS bairroEntrega,
+       IFNULL(T2.fre_amt, 0) / 100                                            AS frete,
+       EO.amount / 100                                                        AS valor,
+       @TIPO                                                                  AS status,
+       IFNULL(A.name, '')                                                     AS area,
+       IFNULL(R.name, '')                                                     AS rota,
+       IF(LEFT(OBS.remarks__480, 2) = 'EF ', LEFT(OBS.remarks__480, 11), ' ') AS obs,
+       A.no                                                                   AS codArea,
+       EO.userno                                                              AS userno,
+       IFNULL(U.name, '')                                                     AS username,
+       CAST(IF(EO.l9 = 0, NULL, EO.l9) AS DATE)                               AS dataPrint,
+       SEC_TO_TIME(IF(EO.l8 = 0, NULL, EO.l8))                                AS horaPrint,
+       EO.l5                                                                  AS userPrint,
+       RPAD(IFNULL(MID(O.remarks__480, 1, 80), ' '), 80, ' ')                 AS obs1,
+       RPAD(IFNULL(MID(O.remarks__480, 81, 80), ' '), 80, ' ')                AS obs2,
+       RPAD(IFNULL(MID(O.remarks__480, 161, 80), ' '), 80, ' ')               AS obs3,
+       RPAD(IFNULL(MID(O.remarks__480, 241, 80), ' '), 80, ' ')               AS obs4,
+       RPAD(IFNULL(MID(O.remarks__480, 321, 80), ' '), 80, ' ')               AS obs5,
+       RPAD(IFNULL(MID(O.remarks__480, 401, 80), ' '), 80, ' ')               AS obs6,
+       RPAD(IFNULL(MID(O.remarks__480, 481, 80), ' '), 80, ' ')               AS obs7,
+       @TIPO                                                                  AS tipo,
+       paym.name                                                              AS metodo,
+       nfe.remarks                                                            AS obsNota
+FROM T_CARGA AS T2
+       LEFT JOIN sqldados.eord AS EO
+                 ON (T2.storeno = EO.storeno AND T2.ordno = EO.ordno)
+       LEFT JOIN sqldados.store AS S
+                 ON S.no = EO.storeno
+       LEFT JOIN sqldados.eordrk AS O
+                 ON (O.storeno = EO.storeno AND O.ordno = EO.ordno)
+       LEFT JOIN sqldados.ctadd AS CA
+                 ON (EO.custno = CA.custno AND CA.seqno = EO.custno_addno)
+       LEFT JOIN sqldados.users AS U
+                 ON U.no = EO.userno
+       LEFT JOIN sqldados.custp AS C
+                 ON (C.no = EO.custno)
+       LEFT JOIN sqldados.emp AS E
+                 ON (E.no = EO.empno)
+       LEFT JOIN sqldados.paym
+                 ON (paym.no = EO.paymno)
+       LEFT JOIN sqlpdv.pxa AS P
+                 ON (EO.storeno = P.storeno AND EO.ordno = P.eordno AND EO.nfno_futura = P.nfno AND
+                     EO.nfse_futura = P.nfse)
+       LEFT JOIN sqldados.ctadd AS AD
+                 ON (C.no = AD.custno AND AD.seqno = EO.custno_addno)
+       LEFT JOIN sqldados.route AS R
+                 ON (AD.routeno = R.no)
+       LEFT JOIN sqldados.area AS A
+                 ON (A.no = R.areano)
+       LEFT JOIN sqldados.nf AS nff
+                 ON (T2.nfno_venda = nff.nfno AND T2.nfse_venda = nff.nfse AND
+                     T2.storeno = nff.storeno)
+       LEFT JOIN sqldados.nf2 AS nff2
+                 ON nff.storeno = nff2.storeno AND nff.pdvno = nff2.pdvno AND nff.xano = nff2.xano
+       LEFT JOIN sqldados.nf AS nfe
+                 ON (T2.nfno_entrega = nfe.nfno AND T2.nfse_entrega = nfe.nfse AND
+                     T2.storeno = nfe.storeno)
+       LEFT JOIN sqldados.nf2 AS nfe2
+                 ON nfe.storeno = nfe2.storeno AND nfe.pdvno = nfe2.pdvno AND nfe.xano = nfe2.xano
+       LEFT JOIN sqldados.eordrk AS OBS
+                 ON (OBS.storeno = EO.storeno AND OBS.ordno = EO.ordno)
+WHERE (EO.date >= @DATA)
+  AND (nff.status <> 1 OR nff.status IS NULL)
+  AND (P.date >= :dataInicial OR :dataInicial = 0)
+  AND (P.date <= :dataFinal OR :dataFinal = 0)
+GROUP BY T2.storeno, T2.ordno;
 
 DROP TEMPORARY TABLE IF EXISTS VENDA_NORMAL;
 CREATE TEMPORARY TABLE VENDA_NORMAL
@@ -299,7 +442,6 @@ GROUP BY EO.storeno, EO.ordno
 HAVING (enderecoEntrega LIKE '%MAGALHAES FILHO%2001%' AND @TIPO = 'R')
     OR (enderecoEntrega NOT LIKE '%MAGALHAES FILHO%2001%' AND @TIPO = 'E');
 
-
 DROP TEMPORARY TABLE IF EXISTS PEDIDOS;
 CREATE TEMPORARY TABLE PEDIDOS
 SELECT loja,
@@ -359,6 +501,64 @@ SELECT loja,
        metodo,
        obsNota
 FROM VENDA_NORMAL AS VN
+UNION
+SELECT loja,
+       nomeLoja,
+       siglaLoja,
+       pedido,
+       separado,
+       zonaCarga,
+       entrega,
+       marca,
+       data,
+       dataEntrega,
+       pdvno,
+       hora,
+       pdvnoVenda,
+       nfnoFat,
+       nfseFat,
+       dataFat,
+       horaFat,
+       valorFat,
+       nfnoEnt,
+       nfseEnt,
+       dataEnt,
+       horaEnt,
+       valorEnt,
+       vendno,
+       vendedor,
+       custno,
+       cliente,
+       foneCliente,
+       cidade,
+       estado,
+       endereco,
+       bairro,
+       enderecoEntrega,
+       bairroEntrega,
+       frete,
+       valor,
+       status,
+       area,
+       rota,
+       obs,
+       codArea,
+       userno,
+       username,
+       dataPrint,
+       horaPrint,
+       userPrint,
+       obs1,
+       obs2,
+       obs3,
+       obs4,
+       obs5,
+       obs6,
+       obs7,
+       tipo,
+       metodo,
+       obsNota
+FROM VENDA_CARGA AS VC
 UNION
 SELECT loja,
        nomeLoja,
