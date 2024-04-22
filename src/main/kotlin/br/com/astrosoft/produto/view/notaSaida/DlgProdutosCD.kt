bf37b51f462cf96cc86295357409b506b1614ae6
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-04-22 17:10:19 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/view/notaSaida/DlgProdutosCD.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/notaSaida/DlgProdutosCD.kt
@@ -14,6 +14,7 @@ import br.com.astrosoft.produto.view.notaSaida.columns.ProdutoNFNFSViewColumns.p
 import br.com.astrosoft.produto.view.notaSaida.columns.ProdutoNFNFSViewColumns.produtoNFPrecoTotal
 import br.com.astrosoft.produto.view.notaSaida.columns.ProdutoNFNFSViewColumns.produtoNFPrecoUnitario
 import br.com.astrosoft.produto.view.notaSaida.columns.ProdutoNFNFSViewColumns.produtoNFQuantidade
+import br.com.astrosoft.produto.view.notaSaida.columns.ProdutoNFNFSViewColumns.produtoNFQuantidadeEnt
 import br.com.astrosoft.produto.view.notaSaida.columns.ProdutoNFNFSViewColumns.produtoNFQuantidadeSaldo
 import br.com.astrosoft.produto.viewmodel.notaSaida.TabNotaCDViewModel
 import com.github.mvysny.karibudsl.v10.button
@@ -135,6 +136,7 @@ class DlgProdutosCD(val viewModel: TabNotaCDViewModel, val nota: NotaSaida) {
       }
       produtoNFLocalizacao()
       produtoNFQuantidade()
+      produtoNFQuantidadeEnt()
       produtoNFQuantidadeSaldo()
       produtoNFPrecoUnitario()
       produtoNFPrecoTotal()
