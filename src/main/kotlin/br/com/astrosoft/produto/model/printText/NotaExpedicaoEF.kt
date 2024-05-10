d7114fed634393d52d83bad693aa32b27a57104b
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-05-10 14:55:24 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/model/printText/NotaExpedicaoEF.kt
+++ src/main/kotlin/br/com/astrosoft/produto/model/printText/NotaExpedicaoEF.kt
@@ -5,13 +5,13 @@ import br.com.astrosoft.framework.model.printText.PrintText
 import br.com.astrosoft.framework.util.format
 import br.com.astrosoft.produto.model.beans.NotaSaida
 import br.com.astrosoft.produto.model.beans.ProdutoNFS
-import br.com.astrosoft.produto.model.beans.ProdutoPedido
 
 class NotaExpedicaoEF(val nota: NotaSaida) : PrintText<ProdutoNFS>() {
   override fun printTitle(bean: ProdutoNFS) {
-    writeln("Romaneio de Separacao para Retira: Reserva ${nota?.numero}", negrito = true, center = true)
+    writeln("Romaneio de Separacao para Entrega: Reserva ${nota?.numero}", negrito = true, center = true)
 
     writeln("<B>Loja: </B>${nota?.loja}")
+    writeln("Zona: ${nota.rota}", negrito = true, expand = true, center = true)
     writeln("<B>Usuario da Impressao: </B>${nota.usuarioEntrega ?: AppConfig.userLogin()?.name ?: ""}")
     writeln("<B>NF de Fatura: </B>${nota?.nota}/${nota?.nota}<B> Data: </B>${nota.data}<B> Hora: </B>${nota.hora}")
     writeln("<B>PDV: </B>${nota.pdvno}      <B> Valor: </B>${nota.valorNota.format()}")
