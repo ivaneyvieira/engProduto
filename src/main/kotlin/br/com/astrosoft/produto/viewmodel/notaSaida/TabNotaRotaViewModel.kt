2754173f8601aae769fff8441cc89896a19ad9ed
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-07-18 14:03:37 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/viewmodel/notaSaida/TabNotaRotaViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/notaSaida/TabNotaRotaViewModel.kt
@@ -106,7 +106,12 @@ class TabNotaRotaViewModel(val viewModel: NotaViewModel) {
 
     report.print(
       dados = listNotaProduto,
-      printer = subView.printerPreview(loja = 0)
+      printer = subView.printerPreview(loja = 0){
+        listNota.forEach { nota ->
+          nota.marcaImpressao()
+        }
+        updateView()
+      }
     )
   }
 
