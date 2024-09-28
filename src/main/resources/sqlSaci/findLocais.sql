31128fd7298719b81add016636cea26327a54022
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-09-28 19:28:09 -0300

    WIP

--- src/main/resources/sqlSaci/findLocais.sql
+++ src/main/resources/sqlSaci/findLocais.sql
@@ -1,4 +1,10 @@
-SELECT MID(localizacao, 1, 4) AS abreviacao
+SELECT DISTINCT MID(localizacao, 1, 4) AS abreviacao
 FROM sqldados.prdloc
 WHERE storeno IN (4)
-GROUP BY abreviacao
+  AND localizacao != ''
+UNION
+DISTINCT
+SELECT DISTINCT MID(localizacao, 1, 4) AS abreviacao
+FROM sqldados.prdAdicional
+WHERE storeno IN (4)
+  AND localizacao != ''
