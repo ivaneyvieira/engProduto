4c2da8bdca7fa92ca1c97e4a219f1cb35aa153bc
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-10-03 17:26:00 -0300

    WIP

--- src/main/resources/sqlSaci/findProdutosNFSaida.sql
+++ src/main/resources/sqlSaci/findProdutosNFSaida.sql
@@ -9,6 +9,7 @@ SELECT A.prdno                                                    AS prdno,
 FROM sqldados.prdAdicional AS A
 WHERE ((MID(A.localizacao, 1, 4) IN (:local)) OR ('TODOS' IN (:local)) OR (A.localizacao != ''))
   AND (A.storeno = :lojaLocal OR :lojaLocal = 0)
+  AND (A.storeno = 4)
   AND (A.prdno = :prdno OR :prdno = '')
   AND (A.grade = :grade OR :grade = '')
 GROUP BY prdno, grade;
