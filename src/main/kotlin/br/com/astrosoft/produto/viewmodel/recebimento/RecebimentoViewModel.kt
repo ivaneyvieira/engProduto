ee09a540da53265b8040f2c3500b70d57f93f965
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-10-09 21:23:22 -0300

    Remoção dos paineis desnecessários do menu Recebimento

--- src/main/kotlin/br/com/astrosoft/produto/viewmodel/recebimento/RecebimentoViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/recebimento/RecebimentoViewModel.kt
@@ -6,18 +6,8 @@ import br.com.astrosoft.framework.viewmodel.ViewModel
 class RecebimentoViewModel(view: IRecebimentoView) : ViewModel<IRecebimentoView>(view) {
   val tabPedidoViewModel = TabPedidoViewModel(this)
   val tabAgendaViewModel = TabAgendaViewModel(this)
-
-  //val tabReceberViewModel = TabReceberViewModel(this)
-  //val tabDevClientesViewModel = TabDevClientesViewModel(this)
-  //val tabReclassificaViewModel = TabReclassificaViewModel(this)
-  //val tabReclassRecViewModel = TabReclassRecViewModel(this)
-  //val tabDevCliRecViewModel = TabDevCliRecViewModel(this)
-  //val tabTransferenciaViewModel = TabTransferenciaViewModel(this)
   val tabReceberNotaViewModel = TabReceberNotaViewModel(this)
   val tabNotaRecebidaViewModel = TabNotaRecebidaViewModel(this)
-
-  //val tabRecebidoViewModel = TabRecebidoViewModel(this)
-  //val tabTransfRecebViewModel = TabTransfRecebViewModel(this)
   val tabRecebimentoUsrViewModel = TabRecebimentoUsrViewModel(this)
 
   override fun listTab() = listOf(
@@ -25,14 +15,6 @@ class RecebimentoViewModel(view: IRecebimentoView) : ViewModel<IRecebimentoView>
     view.tabAgenda,
     view.tabReceberNota,
     view.tabNotaRecebida,
-    //view.tabReceber,
-    //view.tabDevClientes,
-    //view.tabReclassifica,
-    //view.tabTransferencia,
-    //view.tabRecebido,
-    //view.tabDevCliRec,
-    //view.tabReclassRec,
-    //view.tabTransfReceb,
     view.tabRecebimentoUsr,
   )
 }
@@ -40,17 +22,7 @@ class RecebimentoViewModel(view: IRecebimentoView) : ViewModel<IRecebimentoView>
 interface IRecebimentoView : IView {
   val tabPedido: ITabPedido
   val tabAgenda: ITabAgenda
-
-  //val tabReceber: ITabReceber
-  //val tabReclassifica: ITabReclassifica
-  //val tabReclassRec: ITabReclassRec
-  //val tabDevClientes: ITabDevClientes
-  //val tabDevCliRec: ITabDevCliRec
-  //val tabTransferencia: ITabTransferencia
   val tabReceberNota: ITabReceberNota
-
-  //val tabRecebido: ITabRecebido
-  //val tabTransfReceb: ITabTransfReceb
   val tabNotaRecebida: ITabNotaRecebida
   val tabRecebimentoUsr: ITabRecebimentoUsr
 }
