a62049dd5038ec2bd5499f36729f78a2ab265b6c
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2024-12-08 17:00:51 -0300

    Ajuste na deleção de pedidos

--- src/main/resources/sqlSaci/removerPedido.sql
+++ src/main/resources/sqlSaci/removerPedido.sql
@@ -1,17 +1,30 @@
-use sqldados;
+USE sqldados;
 
 DELETE
 FROM ords
 WHERE storeno = 1
   AND no = :ordno;
+
+DELETE
+FROM ordsRessu
+WHERE storeno = 1
+  AND no = :ordno;
+
+DELETE
+FROM oprdRessu
+WHERE storeno = 1
+  AND ordno = :ordno;
+
 DELETE
 FROM orddlv
 WHERE storeno = 1
   AND ordno = :ordno;
+
 DELETE
 FROM oprd
 WHERE storeno = 1
   AND ordno = :ordno;
+
 DELETE
 FROM oprdxf
 WHERE storeno = 1
