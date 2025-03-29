09a7d22493985f1aad66782718c895d73632432e
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-03-29 10:57:12 -0300

    Desfaz as alterações

--- src/main/kotlin/br/com/astrosoft/produto/viewmodel/devFor/DevolucaoAbstractViewModel.kt
+++
@@ -1,8 +0,0 @@
-package br.com.astrosoft.produto.viewmodel.devFor
-
-import br.com.astrosoft.framework.viewmodel.IView
-import br.com.astrosoft.framework.viewmodel.ViewModel
-
-abstract class DevolucaoAbstractViewModel<T : IDevolucaoAbstractView>(view: T) : ViewModel<T>(view)
-
-interface IDevolucaoAbstractView : IView
