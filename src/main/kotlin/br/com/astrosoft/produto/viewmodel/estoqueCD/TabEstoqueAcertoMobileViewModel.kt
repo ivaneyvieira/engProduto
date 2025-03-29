7398f64fc4d4500948e605970997b9eb3a0f528f
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-03-29 13:36:31 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/viewmodel/estoqueCD/TabEstoqueAcertoMobileViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/estoqueCD/TabEstoqueAcertoMobileViewModel.kt
@@ -26,42 +26,10 @@ class TabEstoqueAcertoMobileViewModel(val viewModel: EstoqueCDViewModel) {
 
   fun updateView() = viewModel.exec {
     val filtro = subView.filtro()
-    val produtos = ProdutoEstoqueAcerto.findAll(filtro).agrupa().sortedBy { it.numero }
+    val produtos = ProdutoEstoqueAcerto.findAll(filtro).agrupa().sortedBy { it.numero }.filter {
+      (it.processado == false) && (it.gravado == false)
+    }
     subView.updateProduto(produtos)
-  }
-
-  fun imprimirPedido(acerto: EstoqueAcerto) = viewModel.exec {
-    val filtroVazio = subView.filtroVazio().copy(
-      pedido = acerto.numero,
-      loja = acerto.numloja
-    )
-
-    val produtos = ProdutoEstoque.findProdutoEstoque(filtroVazio)
-
-    if (produtos.isEmpty()) {
-      fail("Nenhum produto selecionado")
-    }
-
-    val report = PrintProdutosConferenciaEstoque2("Pedido de acerto: ${acerto.numero}")
-
-    report.print(
-      dados = produtos, printer = subView.printerPreview()
-    )
-  }
-
-  fun imprimirAcerto(acerto: EstoqueAcerto) = viewModel.exec {
-    val produtos = acerto.findProdutos().filter {
-      (it.diferenca ?: 0) != 0
-    }.sortedBy { it.diferenca ?: 999999 }
-    if (produtos.isEmpty()) {
-      fail("Nenhum produto válido selecionado")
-    }
-
-    val report = PrintProdutosConferenciaAcerto()
-
-    report.print(
-      dados = produtos, printer = subView.printerPreview()
-    )
   }
 
   fun cancelarAcerto() = viewModel.exec {
@@ -77,13 +45,6 @@ class TabEstoqueAcertoMobileViewModel(val viewModel: EstoqueCDViewModel) {
       }
       updateView()
     }
-  }
-
-  fun imprimirRelatorio(acerto: EstoqueAcerto) {
-    val produtos = acerto.findProdutos()
-    val report = ReportAcerto()
-    val file = report.processaRelatorio(produtos)
-    viewModel.view.showReport(chave = "Acerto${System.nanoTime()}", report = file)
   }
 
   fun geraPlanilha(produtos: List<ProdutoEstoqueAcerto>): ByteArray {
