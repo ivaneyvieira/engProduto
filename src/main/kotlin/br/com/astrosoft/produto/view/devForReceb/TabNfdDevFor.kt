fc38dc2d50012f9dc22fdceb7ef33ff259c550cb
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-05-10 09:35:17 -0300

    Copia do painel NddDev para o Menu Dev For

---
+++ src/main/kotlin/br/com/astrosoft/produto/view/devForReceb/TabNfdDevFor.kt
@@ -0,0 +1,175 @@
+package br.com.astrosoft.produto.view.devForReceb
+
+import br.com.astrosoft.framework.model.config.AppConfig
+import br.com.astrosoft.framework.view.vaadin.TabPanelGrid
+import br.com.astrosoft.framework.view.vaadin.helper.addColumnButton
+import br.com.astrosoft.framework.view.vaadin.helper.columnGrid
+import br.com.astrosoft.framework.view.vaadin.helper.format
+import br.com.astrosoft.framework.view.vaadin.helper.localePtBr
+import br.com.astrosoft.produto.model.beans.*
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaHora
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFCliente
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFData
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFEntregaRetira
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFLoja
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFNota
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFSituacao
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFTipo
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFValor
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNFVendedor
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaNomeCliente
+import br.com.astrosoft.produto.view.nfd.columns.NotaColumns.colunaRota
+import br.com.astrosoft.produto.viewmodel.nfd.ITabNfdDevFor
+import br.com.astrosoft.produto.viewmodel.nfd.TabNfdDevForViewModel
+import com.github.mvysny.karibudsl.v10.datePicker
+import com.github.mvysny.karibudsl.v10.select
+import com.github.mvysny.karibudsl.v10.textField
+import com.vaadin.flow.component.datepicker.DatePicker
+import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.select.Select
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.value.ValueChangeMode
+import java.time.LocalDate
+
+class TabNfdDevFor(val viewModel: TabNfdDevForViewModel) : TabPanelGrid<NotaSaida>(NotaSaida::class), ITabNfdDevFor {
+  //private var colRota: Grid.Column<NotaSaida>? = null
+  private var dlgProduto: DlgProdutosDevFor? = null
+  private lateinit var cmbLoja: Select<Loja>
+  private lateinit var edtDataInicial: DatePicker
+  private lateinit var edtDataFinal: DatePicker
+  private lateinit var edtPesquisa: TextField
+
+  fun init() {
+    val user = AppConfig.userLogin() as? UserSaci
+    val loja = user?.lojaNota ?: 0
+    val lojaSelecionada = viewModel.findAllLojas().firstOrNull { it.no == loja }
+    cmbLoja.isReadOnly = (user?.admin == false) && (lojaSelecionada?.no != 0)
+    cmbLoja.setItems(viewModel.findAllLojas() + listOf(Loja.lojaZero))
+    cmbLoja.value = lojaSelecionada ?: Loja.lojaZero
+  }
+
+  override fun HorizontalLayout.toolBarConfig() {
+    cmbLoja = select("Loja") {
+      this.setItemLabelGenerator { item ->
+        item.descricao
+      }
+      addValueChangeListener {
+        if (it.isFromClient)
+          viewModel.updateView()
+      }
+    }
+    init()
+
+    edtPesquisa = textField("Pesquisa") {
+      this.valueChangeMode = ValueChangeMode.LAZY
+      this.valueChangeTimeout = 1500
+      addValueChangeListener {
+        viewModel.updateView()
+      }
+    }
+    edtDataInicial = datePicker("Data Inicial") {
+      this.localePtBr()
+      this.value = LocalDate.now()
+      addValueChangeListener {
+        viewModel.updateView()
+      }
+    }
+    edtDataFinal = datePicker("Data Final") {
+      this.localePtBr()
+      this.value = LocalDate.now()
+      addValueChangeListener {
+        viewModel.updateView()
+      }
+    }
+  }
+
+  override fun Grid<NotaSaida>.gridPanel() {
+    this.addClassName("styling")
+    this.format()
+
+    colunaNFLoja()
+
+    columnGrid(NotaSaida::usuarioSingExp, "Autoriza")
+
+    addColumnButton(VaadinIcon.FILE_TABLE, "Produtos", "Produtos") { nota ->
+      dlgProduto = DlgProdutosDevFor(viewModel, nota)
+      dlgProduto?.showDialog {
+        viewModel.updateView()
+      }
+    }
+    colunaNFNota()
+    colunaNFData()
+    //colunaHora()
+    //colRota = colunaRota()
+    colunaNFCliente()
+    colunaNomeCliente()
+    //colunaNFVendedor()
+    //colunaNomeVendedor()
+    colunaNFValor()
+    colunaNFTipo()
+    //colunaNFEntregaRetira()
+    //colunaNFSituacao()
+
+    this.setPartNameGenerator {
+      val countEnt = it.countEnt ?: 0
+      val countImp = it.countImp ?: 0
+      val cancelada = it.cancelada ?: "N"
+      when {
+        cancelada == "S" -> "vermelho"
+
+        countImp > 0 -> "azul"
+
+        countEnt > 0 -> "amarelo"
+
+        else -> null
+      }
+    }
+  }
+
+  override fun filtro(marca: EMarcaNota): FiltroNota {
+    return FiltroNota(
+      marca = marca,
+      tipoNota = ETipoNotaFiscal.DEVOLUCAO,
+      loja = cmbLoja.value?.no ?: 0,
+      dataInicial = edtDataInicial.value,
+      dataFinal = edtDataFinal.value,
+      todosLocais = true,
+      pesquisa = edtPesquisa.value ?: "",
+    )
+  }
+
+  override fun updateNotas(notas: List<NotaSaida>) {
+    updateGrid(notas)
+  }
+
+  override fun findNota(): NotaSaida? {
+    return dlgProduto?.nota
+  }
+
+  override fun updateProdutos() {
+    dlgProduto?.update()
+  }
+
+  override fun produtosSelcionados(): List<ProdutoNFS> {
+    return dlgProduto?.itensSelecionados().orEmpty()
+  }
+
+  override fun isAuthorized(): Boolean {
+    val username = AppConfig.userLogin() as? UserSaci
+    return username?.nfdDevFor == true
+  }
+
+  override val label: String
+    get() = "Dev For"
+
+  override fun updateComponent() {
+    viewModel.updateView()
+  }
+
+  override fun printerUser(): List<String> {
+    val user = AppConfig.userLogin() as? UserSaci
+    return user?.impressoraNotaTermica?.toList().orEmpty()
+  }
+}
---
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/devForRecebe/TabNddDevForViewModel.kt
@@ -0,0 +1,88 @@
+package br.com.astrosoft.produto.viewmodel.devForRecebe
+
+import br.com.astrosoft.framework.model.config.AppConfig
+import br.com.astrosoft.framework.viewmodel.ITabView
+import br.com.astrosoft.framework.viewmodel.fail
+import br.com.astrosoft.produto.model.beans.*
+import br.com.astrosoft.produto.model.printText.NotaExpedicao
+import br.com.astrosoft.produto.model.printText.NotaExpedicaoEF
+import br.com.astrosoft.produto.model.zpl.EtiquetaChave
+import br.com.astrosoft.produto.viewmodel.nfd.NfdViewModel
+
+class TabNfdDevForViewModel(val viewModel: NfdViewModel) {
+  fun findAllLojas(): List<Loja> {
+    return Loja.allLojas()
+  }
+
+  fun updateView() {
+    val user = AppConfig.userLogin() as? UserSaci
+    val marca = if (user?.admin == true)
+      EMarcaNota.TODOS
+    else
+      EMarcaNota.EXP
+    val filtro = subView.filtro(marca)
+    val notas = NotaSaida.find(filtro)
+    subView.updateNotas(notas)
+  }
+
+  fun findGrade(prd: ProdutoNFS?, block: (List<PrdGrade>) -> Unit) = viewModel.exec {
+    prd ?: return@exec
+    val list = prd.findGrades()
+    block(list)
+  }
+
+  private fun imprimeEtiqueta(produtos: List<ProdutoNFS>) {
+    val user = AppConfig.userLogin() as? UserSaci
+    user?.impressoraNota?.let { impressora ->
+      try {
+        EtiquetaChave.printPreviewExp(impressora, produtos, 1)
+      } catch (e: Throwable) {
+        e.printStackTrace()
+        fail("Falha de impressão na impressora $impressora")
+      }
+    }
+  }
+
+  fun imprimeProdutosNota(nota: NotaSaida, itensSelecionados: List<ProdutoNFS>) = viewModel.exec {
+    if (itensSelecionados.isEmpty())
+      fail("Nenhum produto selecionado")
+    if (nota.cancelada == "S")
+      fail("Nota cancelada")
+    val tipo = nota.tipoNotaSaida ?: ""
+    val report = if (tipo == "ENTRE_FUT") NotaExpedicaoEF(nota) else NotaExpedicao(nota)
+    report.print(
+      dados = itensSelecionados,
+      printer = subView.printerPreview(loja = nota.loja),
+    )
+  }
+
+  fun autorizaProduto(listaPrd: List<ProdutoNFS>, login: String, senha: String): UserSaci? {
+    val lista = UserSaci.findAll()
+    val user = lista
+      .firstOrNull {
+        it.login.uppercase() == login.uppercase() && it.senha.uppercase().trim() == senha.uppercase().trim()
+      }
+
+    if (user == null) {
+      viewModel.view.showError("Usuário ou senha inválidos")
+    } else {
+      listaPrd.forEach { produto ->
+        produto.usernoExp = user.no
+        produto.salva()
+      }
+    }
+
+    return user
+  }
+
+  val subView
+    get() = viewModel.view.tabNfdDevFor
+}
+
+interface ITabNfdDevFor : ITabView {
+  fun filtro(marca: EMarcaNota): FiltroNota
+  fun updateNotas(notas: List<NotaSaida>)
+  fun findNota(): NotaSaida?
+  fun updateProdutos()
+  fun produtosSelcionados(): List<ProdutoNFS>
+}
