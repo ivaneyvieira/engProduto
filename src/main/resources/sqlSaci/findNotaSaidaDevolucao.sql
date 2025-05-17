cdad270d6a4d0baefadd882952d4df928ee68664
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-05-17 10:20:55 -0300

    WIP

--- src/main/resources/sqlSaci/findNotaSaidaDevolucao.sql
+++ src/main/resources/sqlSaci/findNotaSaidaDevolucao.sql
@@ -27,9 +27,9 @@ SELECT N.storeno                                                              AS
        CAST(IF(N.l16 = 0, NULL, N.l16) AS DATE)                               AS entrega,
        print_remarks                                                          AS observacaoPrint,
        CASE D.status
-         WHEN 0 THEN 'Incluida'
+         WHEN 0 THEN 'Incluída'
          WHEN 1 THEN 'Em cobrança'
-         WHEN 2 THEN 'Quitada"'
+         WHEN 2 THEN 'Quitada'
          WHEN 3 THEN 'Cartório'
          WHEN 4 THEN 'No advogado'
          WHEN 5 THEN 'Cancelada'
