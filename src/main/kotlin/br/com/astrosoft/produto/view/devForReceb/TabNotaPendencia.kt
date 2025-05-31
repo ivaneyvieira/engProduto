b7d3a06e39f41df9a86911f25ea8d431abd9d03d
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-05-31 08:58:12 -0300

    Ajustes no painel pendencia

--- src/main/kotlin/br/com/astrosoft/produto/view/devForReceb/TabNotaPendencia.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/devForReceb/TabNotaPendencia.kt
@@ -124,20 +124,20 @@ class TabNotaPendencia(val viewModel: TabNotaPendenciaViewModel) :
     this.removeThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT)
 
     columnGrid(NotaRecebimentoDev::tipoDevolucaoName, header = "Motivo Devolução")
-   // columnGrid(NotaRecebimentoDev::dataDevolucao, header = "Data", width = null).dateFieldEditor()
+    // columnGrid(NotaRecebimentoDev::dataDevolucao, header = "Data", width = null).dateFieldEditor()
     columnGrid(NotaRecebimentoDev::numeroDevolucao, header = "Pedido").right()
     columnGrid(NotaRecebimentoDev::valorNFDevolucao, header = "Valor Ped")
-    columnGrid(NotaRecebimentoDev::notaDevolucao, header = "NFD", width = null)
-    columnGrid(NotaRecebimentoDev::emissaoDevolucao, header = "Emissão", width = null)
+    columnGrid(NotaRecebimentoDev::notaDevolucao, header = "NFD", width = "7rem")
+    columnGrid(NotaRecebimentoDev::emissaoDevolucao, header = "Emissão", width = "7rem")
     columnGrid(NotaRecebimentoDev::valorDevolucao, header = "Valor NFD", width = null)
     columnGrid(NotaRecebimentoDev::vendnoNF, header = "For NF")
     columnGrid(NotaRecebimentoDev::fornecedorNF, header = "Nome Fornecedor")
-    columnGrid(NotaRecebimentoDev::niListStr, header = "NI", width = "5.5rem")
-    columnGrid(NotaRecebimentoDev::nfEntrada, header = "NF Entrada").right()
-    columnGrid(NotaRecebimentoDev::emissao, header = "Emissão", width = null)
-    columnGrid(NotaRecebimentoDev::dataEntrada, header = "Entrada", width = null)
+    //columnGrid(NotaRecebimentoDev::niListStr, header = "NI", width = "5.5rem")
+    //columnGrid(NotaRecebimentoDev::nfEntrada, header = "NF Entrada").right()
+    //columnGrid(NotaRecebimentoDev::emissao, header = "Emissão", width = null)
+    //columnGrid(NotaRecebimentoDev::dataEntrada, header = "Entrada", width = null)
     columnGrid(NotaRecebimentoDev::userDevolucao, header = "Usuário")
-    columnGrid(NotaRecebimentoDev::observacaoDev, header = "Observação", width = "200px").textFieldEditor()
+    columnGrid(NotaRecebimentoDev::observacaoDev, header = "Observação", isExpand = true).textFieldEditor()
   }
 
   override fun filtro(): FiltroNotaRecebimentoProdutoDev {
