fcfda96d7891a67344d255c82b3e1dcd23d686ce
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-07-18 15:52:39 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/model/printText/PrintTermoCupom.kt
+++ src/main/kotlin/br/com/astrosoft/produto/model/printText/PrintTermoCupom.kt
@@ -8,42 +8,43 @@ import java.time.LocalDate
 
 class PrintTermoCupom() : PrintText<TermoRecebimento>() {
   override fun printTitle(bean: TermoRecebimento) {
-    writeln(bean.dadosCliente.nome, negrito = true)
-    writeln("CNPJ: ${bean.dadosCliente.cnpj}", negrito = true)
-    writeln("End: ${bean.dadosCliente.endereco} Bairro ${bean.dadosCliente.bairro}", negrito = true)
-    writeln("Cidade: ${bean.dadosCliente.cidade} Estado: ${bean.dadosCliente.uf}", negrito = true)
-    writeln("E-mail: ${bean.empEmailTermo}", negrito = true)
-    writeln("WhatApp: (86) 99978-0752", negrito = true)
+    writeln("")
+    writeln("  ${bean.dadosCliente.nome}", negrito = true)
+    writeln("  CNPJ: ${bean.dadosCliente.cnpj}", negrito = true)
+    writeln("  End: ${bean.dadosCliente.endereco} Bairro ${bean.dadosCliente.bairro}", negrito = true)
+    writeln("  Cidade: ${bean.dadosCliente.cidade} Estado: ${bean.dadosCliente.uf}", negrito = true)
+    writeln("  E-mail: ${bean.empEmailTermo}", negrito = true)
+    writeln("  WhatApp: (86) 99978-0752", negrito = true)
     writeln("")
     val volumeRec = bean.volumesRec?.toString() ?: "0"
     writeln("Termo de Recebimento de Volume - NI ${bean.ni}", negrito = true, center = true)
     writeln("")
-    writeln("      Declaramos para os devidos fins que recebemos ${volumeRec.lpad(4, " ")} volumes")
-    writeln("que constam na NF-e e CT-e informados abaixo,  para  conferencia")
-    writeln("posterior  e  sujeitos  as  notificacoes  de  irregularidade  no")
-    writeln("recebimento.")
+    writeln("        Declaramos para os  devidos  fins  que  recebemos ${volumeRec.lpad(4, " ")}")
+    writeln("  volumes que constam na NF-e e CT-e informados  abaixo,  para")
+    writeln("  conferencia  posterior  e  sujeitos   as   notificacoes   de")
+    writeln("  irregularidade  no recebimento.")
     writeln("")
-    writeln("Fornecedor: ${bean.dadosFornecedor.nome}")
-    writeln("CNPJ: ${bean.dadosFornecedor.cnpj}")
+    writeln("  Fornecedor: ${bean.dadosFornecedor.nome}")
+    writeln("  CNPJ: ${bean.dadosFornecedor.cnpj}")
 
     val notaFiscal = "Nota Fiscal: ${bean.dadosFornecedor.notaFiscal}"
     val emissao = "Emissão: ${bean.dadosFornecedor.emissao?.format() ?: ""}"
     val valor = "Valor: ${bean.dadosFornecedor.valor.format()}"
 
-    val espacoResto = 64 - (notaFiscal.length + valor.length + emissao.length)
+    val espacoResto = 60 - (notaFiscal.length + valor.length + emissao.length)
     val espaco1 = espacoResto / 2
 
-    writeln("${notaFiscal}${" ".repeat(espaco1)}$emissao")
+    writeln("  ${notaFiscal}${" ".repeat(espaco1)}$emissao")
 
-    val volume = "Volumes: ${bean.volumesInf?.format() ?: ""}"
+    val volume = "  Volumes: ${bean.volumesInf?.format() ?: ""}"
     writeln(volume)
     writeln("")
-    writeln("Transportadora: ${bean.dadosTransportadora.nome}")
-    writeln("CNPJ: ${bean.dadosTransportadora.cnpj}")
+    writeln("  Transportadora: ${bean.dadosTransportadora.nome}")
+    writeln("  CNPJ: ${bean.dadosTransportadora.cnpj}")
     val cte = "CT-e: ${bean.dadosTransportadora.cte}"
     val emissaoTransp = "Emissão: ${bean.dadosTransportadora.emissao?.format() ?: ""}"
     val espacoResto3 = (64 - (cte.length + emissaoTransp.length)) / 2
-    writeln("$cte${" ".repeat(espacoResto3)}$emissaoTransp")
+    writeln("  $cte${" ".repeat(espacoResto3)}$emissaoTransp")
   }
 
   override fun printSumary(bean: TermoRecebimento?) {
