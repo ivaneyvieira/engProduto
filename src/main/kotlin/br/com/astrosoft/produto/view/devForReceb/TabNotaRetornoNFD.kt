4efcda3b46e7513224f791953889d1cf2a89fef0
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-08-11 22:48:39 -0300

    Copia painel Retorno NFD no menu Dev Fornecedor

---
+++ src/main/kotlin/br/com/astrosoft/produto/view/devForReceb/TabNotaRetornoNFD.kt
@@ -0,0 +1,173 @@
+package br.com.astrosoft.produto.view.devForReceb
+
+import br.com.astrosoft.framework.model.config.AppConfig
+import br.com.astrosoft.framework.view.vaadin.TabPanelGrid
+import br.com.astrosoft.framework.view.vaadin.helper.*
+import br.com.astrosoft.produto.model.beans.*
+import br.com.astrosoft.produto.viewmodel.devForRecebe.ITabNotaEmail
+import br.com.astrosoft.produto.viewmodel.devForRecebe.TabNotaEmailViewModel
+import com.github.mvysny.karibudsl.v10.button
+import com.github.mvysny.karibudsl.v10.onClick
+import com.github.mvysny.karibudsl.v10.select
+import com.github.mvysny.karibudsl.v10.textField
+import com.github.mvysny.kaributools.getColumnBy
+import com.vaadin.flow.component.Focusable
+import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.grid.GridVariant
+import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.select.Select
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.value.ValueChangeMode
+
+class TabNotaRetornoNFD(val viewModel: TabNotaEmailViewModel) :
+  TabPanelGrid<NotaRecebimentoDev>(NotaRecebimentoDev::class), ITabNotaEmail {
+  private var dlgProduto: DlgProdutosNotaEmail? = null
+  private var dlgArquivo: DlgArquivoNotaEmail? = null
+  private lateinit var cmbLoja: Select<Loja>
+  private lateinit var edtPesquisa: TextField
+
+  fun init() {
+    val user = AppConfig.userLogin() as? UserSaci
+    val lojaUSer = user?.devFor2Loja ?: 0
+    val lojas = if (lojaUSer == 0) {
+      viewModel.findAllLojas() + listOf(Loja.lojaZero)
+    } else {
+      viewModel.findAllLojas().filter { it.no == lojaUSer }
+    }
+    cmbLoja.setItems(lojas)
+    cmbLoja.value = lojas.firstOrNull { it.no == lojaUSer }
+  }
+
+  override fun HorizontalLayout.toolBarConfig() {
+    cmbLoja = select("Loja") {
+      this.setItemLabelGenerator { item ->
+        item.descricao
+      }
+      addValueChangeListener {
+        if (it.isFromClient)
+          viewModel.updateView()
+      }
+    }
+    init()
+    edtPesquisa = textField("Pesquisa") {
+      this.width = "300px"
+      this.valueChangeMode = ValueChangeMode.LAZY
+      this.valueChangeTimeout = 1500
+      addValueChangeListener {
+        viewModel.updateView()
+      }
+    }
+
+    button("Pedido") {
+      this.icon = VaadinIcon.ARROW_LEFT.create()
+      this.onClick {
+        viewModel.marcaSituacao(EStituacaoDev.PEDIDO)
+      }
+    }
+  }
+
+  override fun Grid<NotaRecebimentoDev>.gridPanel() {
+    this.addClassName("styling")
+    this.format()
+
+    this.withEditor(
+      classBean = NotaRecebimentoDev::class,
+      openEditor = {
+        val edit = getColumnBy(NotaRecebimentoDev::observacaoDev) as? Focusable<*>
+        edit?.focus()
+      },
+      closeEditor = {
+        viewModel.saveNota(nota = it.bean, updateGrid = true)
+      })
+
+    columnGrid(NotaRecebimentoDev::loja, header = "Loja")
+
+    addColumnButton(VaadinIcon.FILE_TABLE, "Produtos", "Produtos") { nota ->
+      dlgProduto = DlgProdutosNotaEmail(viewModel, nota)
+      dlgProduto?.showDialog {
+        viewModel.updateView()
+      }
+    }
+
+    addColumnButton(VaadinIcon.FILE, "Arquivo", "Arquivo", configIcon = { icon, bean ->
+      if (bean.countArq?.let { it > 0 } == true) {
+        icon.element.style.set("color", "yellow")
+      }
+    }) { nota ->
+      dlgArquivo = DlgArquivoNotaEmail(viewModel, nota)
+      dlgArquivo?.showDialog {
+        viewModel.updateView()
+      }
+    }
+
+    this.selectionMode = Grid.SelectionMode.MULTI
+
+    this.removeThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT)
+
+    columnGrid(NotaRecebimentoDev::motivoDevolucaoName, header = "Motivo Devolução")
+    columnGrid(NotaRecebimentoDev::numeroDevolucao, header = "Pedido").right()
+    columnGrid(NotaRecebimentoDev::notaDevolucao, header = "NFD", width = null)
+    columnGrid(NotaRecebimentoDev::emissaoDevolucao, header = "Emissão", width = null)
+    columnGrid(NotaRecebimentoDev::valorDevolucao, header = "Valor Nota", width = null)
+    columnGrid(NotaRecebimentoDev::vendnoNF, header = "For NF")
+    columnGrid(NotaRecebimentoDev::fornecedorNF, header = "Nome Fornecedor")
+    columnGrid(NotaRecebimentoDev::userDevolucao, header = "Usuário")
+    columnGrid(NotaRecebimentoDev::observacaoDev, header = "Observação", width = "200px").textFieldEditor()
+  }
+
+  override fun filtro(): FiltroNotaRecebimentoProdutoDev {
+    return FiltroNotaRecebimentoProdutoDev(
+      loja = cmbLoja.value?.no ?: 0,
+      pesquisa = edtPesquisa.value ?: "",
+    )
+  }
+
+  override fun updateNota(notas: List<NotaRecebimentoDev>) {
+    this.updateGrid(notas)
+  }
+
+  override fun updateArquivos() {
+    dlgArquivo?.update()
+  }
+
+  override fun arquivosSelecionados(): List<InvFileDev> {
+    return dlgArquivo?.produtosSelecionados().orEmpty()
+  }
+
+  override fun produtosSelecionados(): List<NotaRecebimentoProdutoDev> {
+    return this.dlgProduto?.produtosSelecionados().orEmpty()
+  }
+
+  override fun notasSelecionadas(): List<NotaRecebimentoDev> {
+    return this.itensSelecionados()
+  }
+
+  override fun updateProduto(): NotaRecebimentoDev? {
+    return dlgProduto?.updateProduto()
+  }
+
+  fun showDlgProdutos(nota: NotaRecebimentoDev) {
+    dlgProduto = DlgProdutosNotaEmail(viewModel, nota)
+    dlgProduto?.showDialog {
+      viewModel.updateView()
+    }
+  }
+
+  override fun isAuthorized(): Boolean {
+    val username = AppConfig.userLogin() as? UserSaci
+    return username?.devFor2NotaEmail == true
+  }
+
+  override val label: String
+    get() = "E-mail"
+
+  override fun updateComponent() {
+    viewModel.updateView()
+  }
+
+  override fun printerUser(): List<String> {
+    val user = AppConfig.userLogin() as? UserSaci
+    return user?.impressoraRec.orEmpty().toList()
+  }
+}
