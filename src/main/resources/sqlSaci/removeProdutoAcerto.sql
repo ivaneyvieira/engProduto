f6befe6503f7b37b087083bd7329080ff2ea210b
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-09-02 05:38:24 -0300

    Ajuste na remoção de produtos no painel de acerto

--- src/main/resources/sqlSaci/removeProdutoAcerto.sql
+++ src/main/resources/sqlSaci/removeProdutoAcerto.sql
@@ -5,8 +5,7 @@ WHERE storeno = :loja
   AND ordno = :pedido
   AND prdno = :prdno
   AND grade = :grade
-  AND :loja != 1
-  AND :pedido != 2;
+  AND (:loja != 1 OR :pedido != 2);
 
 UPDATE sqldados.oprd
 SET qtty = IF(:lojaAcerto = 0, 0, REPLACE(qtty, :lojaAcerto, '') * 1)
@@ -14,5 +13,4 @@ WHERE storeno = :loja
   AND ordno = :pedido
   AND prdno = :prdno
   AND grade = :grade
-  AND :loja = 1
-  AND :pedido = 2
+  AND (:loja = 1 AND :pedido = 2)
