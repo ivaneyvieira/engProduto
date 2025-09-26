8d3daebd2787976ef378e760ccdfff2eaabc6e40
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-09-26 13:56:38 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/view/estoqueCD/DlgProdutoKardec.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/estoqueCD/DlgProdutoKardec.kt
@@ -8,15 +8,23 @@ import br.com.astrosoft.framework.view.vaadin.helper.right
 import br.com.astrosoft.produto.model.beans.ProdutoEstoque
 import br.com.astrosoft.produto.model.beans.ProdutoKardec
 import br.com.astrosoft.produto.viewmodel.estoqueCD.TabEstoqueSaldoViewModel
+import com.github.mvysny.karibudsl.v10.textField
 import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.grid.GridVariant
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.value.ValueChangeMode
 import java.time.LocalDate
 
-class DlgProdutoKardec(val viewModel: TabEstoqueSaldoViewModel, val produto: ProdutoEstoque, val dataIncial: LocalDate?) {
+class DlgProdutoKardec(
+  val viewModel: TabEstoqueSaldoViewModel,
+  val produto: ProdutoEstoque,
+  val dataIncial: LocalDate?
+) {
   private var onClose: (() -> Unit)? = null
   private var form: SubWindowForm? = null
   private val gridDetail = Grid(ProdutoKardec::class.java, false)
+  private lateinit var edtPesquisa: TextField
 
   fun showDialog(onClose: () -> Unit) {
     this.onClose = onClose
@@ -33,6 +41,14 @@ class DlgProdutoKardec(val viewModel: TabEstoqueSaldoViewModel, val produto: Pro
     form = SubWindowForm(
       "$codigo $descricao$grade ($localizacao) Data Inicial: ${dataInicial.format()} Estoque: ${produto.saldo ?: 0}",
       toolBar = {
+        edtPesquisa = textField("Psquisa") {
+          this.width = "300px"
+          this.valueChangeMode = ValueChangeMode.LAZY
+          this.valueChangeTimeout = 1500
+          addValueChangeListener {
+            update()
+          }
+        }
       },
       onClose = {
         closeForm()
@@ -75,7 +91,24 @@ class DlgProdutoKardec(val viewModel: TabEstoqueSaldoViewModel, val produto: Pro
   }
 
   fun update() {
-    val kardec = viewModel.kardec(produto, dataIncial)
+    val kardec = viewModel.kardec(produto, dataIncial).filter {
+      val pesquisa = edtPesquisa.value?.trim() ?: ""
+      if (pesquisa.isEmpty()) return@filter true
+      val doc = it.doc ?: ""
+      val obs = it.observacao ?: ""
+      val data = it.data?.format() ?: ""
+      val user = it.userLogin ?: ""
+      val nfEnt = it.nfEnt
+      val tipo = it.tipoDescricao
+      doc.contains(pesquisa, ignoreCase = true) || obs.contains(
+        pesquisa,
+        ignoreCase = true
+      ) || data.startsWith(pesquisa)
+      || user.contains(pesquisa, ignoreCase = true) || nfEnt.toString() == pesquisa || tipo.contains(
+        pesquisa,
+        ignoreCase = true
+      )
+    }
     gridDetail.setItems(kardec)
   }
 
