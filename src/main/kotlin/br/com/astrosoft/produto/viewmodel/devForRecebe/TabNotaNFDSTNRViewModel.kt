ee89870f5b4285e2bf494475fed4523ed9a34d29
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-09-30 19:44:17 -0300

    Copia painel NFD ST/NR

---
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/devForRecebe/TabNotaNFDSTNRViewModel.kt
@@ -0,0 +1,43 @@
+package br.com.astrosoft.produto.viewmodel.devForRecebe
+
+import br.com.astrosoft.framework.viewmodel.ITabView
+import br.com.astrosoft.produto.model.beans.FiltroFornecedor
+import br.com.astrosoft.produto.model.beans.FornecedorArquivo
+import br.com.astrosoft.produto.model.beans.FornecedorClass
+
+class TabNotaNFDSTNRViewModel(val viewModel: DevFor2ViewModel) {
+  val subView
+    get() = viewModel.view.tabNotaNFDSTNR
+
+  fun updateView() {
+    val filtro = subView.filtro()
+    val notas = FornecedorClass.findAll(filtro = filtro)
+    subView.updateFornecedor(notas)
+  }
+
+  fun saveForne(forn: FornecedorClass) {
+    forn.save()
+    updateView()
+  }
+
+  fun addArquivo(fornecedor: FornecedorClass, fileName: String, dados: ByteArray) {
+    val vendno = fornecedor.no ?: return
+    FornecedorArquivo.save(vendno, fileName, dados)
+    subView.updateArquivos()
+  }
+
+  fun removeArquivosSelecionado() {
+    val selecionado = subView.arquivosSelecionados()
+    selecionado.forEach {
+      it.remove()
+    }
+    subView.updateArquivos()
+  }
+}
+
+interface ITabNotaNFDSTNR : ITabView {
+  fun filtro(): FiltroFornecedor
+  fun updateFornecedor(fornecedore: List<FornecedorClass>)
+  fun updateArquivos()
+  fun arquivosSelecionados(): List<FornecedorArquivo>
+}
