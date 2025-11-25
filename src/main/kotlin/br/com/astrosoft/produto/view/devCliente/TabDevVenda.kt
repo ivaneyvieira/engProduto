145437cfae47c96b4c65b864260854fe7e7fd51e
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-11-25 08:41:41 -0300

    Inibe o botão autoriza venda

--- src/main/kotlin/br/com/astrosoft/produto/view/devCliente/TabDevVenda.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/devCliente/TabDevVenda.kt
@@ -90,12 +90,13 @@ class TabDevVenda(val viewModel: TabDevCliVendaViewModel) :
       val vendas = itensSelecionados()
       viewModel.geraPlanilha(vendas)
     }
+    /*
     button("Autoriza") {
       icon = VaadinIcon.KEY.create()
       onClick {
         viewModel.autorizaTroca()
       }
-    }
+    }*/
   }
 
   override fun Grid<NotaVenda>.gridPanel() {
