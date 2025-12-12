4c828ce5c38e61a61fbbd9cb995fa83ea19b4f4b
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2025-12-12 19:54:20 -0300

    Copia painel Controle Loja

---
+++ src/main/kotlin/br/com/astrosoft/produto/model/beans/ProdutoControle.kt
@@ -0,0 +1,455 @@
+package br.com.astrosoft.produto.model.beans
+
+import br.com.astrosoft.framework.util.lpad
+import br.com.astrosoft.framework.util.toSaciDate
+import br.com.astrosoft.produto.model.beans.UserSaci.Companion.userRessuprimentoLocais
+import br.com.astrosoft.produto.model.saci
+import java.time.LocalDate
+import kotlin.math.roundToInt
+
+class ProdutoEstoque(
+  var loja: Int?,
+  var lojaSigla: String?,
+  var prdno: String?,
+  var codigo: Int?,
+  var descricao: String?,
+  var unidade: String?,
+  var grade: String?,
+  var tipo: Int?,
+  var cl: Int?,
+  var embalagem: Int?,
+  var qtdEmbalagem: Double?,
+  var estoque: Int?,
+  var locNerus: String?,
+  var locApp: String?,
+  var codForn: Int?,
+  var fornecedor: String?,
+  var fornecedorAbrev: String?,
+  var cnpjFornecedor: String?,
+  var saldo: Int?,
+  var valorEstoque: Double?,
+  var saldoVarejo: Int?,
+  var saldoAtacado: Int?,
+  var dataInicial: LocalDate?,
+  var dataUpdate: LocalDate?,
+  var kardec: Int? = null,
+  var dataConferencia: LocalDate? = null,
+  var qtConfEditLoja: Int? = null,
+  var qtConfEdit: Int? = null,
+  var qtConferencia: Int? = null,
+  var preco: Double? = null,
+  var estoqueUser: Int? = null,
+  var estoqueLogin: String? = null,
+  var estoqueData: LocalDate? = null,
+  var estoqueCD: Int? = null,
+  var estoqueLoja: Int? = null,
+  var barcode: String? = null,
+  var ref: String? = null,
+  var numeroAcerto: Int? = null,
+  var processado: Boolean? = false,
+  var estoqueConfCD: Int? = null,
+  var estoqueConfLoja: Int? = null,
+) {
+  val qtConfCalc: Int
+    get() = (qtConfEdit ?: 0) + (qtConfEditLoja ?: 0)
+
+  val qtConfCalcEstoque: Int
+    get() = (estoqueConfCD ?: 0) + (estoqueConfLoja ?: 0)
+
+  val qtDifCalcEstoque: Int
+    get() = qtConfCalcEstoque - (saldo ?: 0)
+
+  fun isUpdated(): Boolean {
+    return dataUpdate.toSaciDate() == LocalDate.now().toSaciDate()
+  }
+
+  val qtdDif: Double
+    get() {
+      val sistema = saldo?.toDouble() ?: 0.0
+      val cd = kardec?.toDouble() ?: 0.0
+      val diferenca = sistema - cd
+      return if (descricao?.startsWith("SVS E") == true) {
+        diferenca * 900.0 / 900.0
+      } else {
+        diferenca
+      }
+    }
+
+  val qtdDifInt
+    get() = qtdDif.roundToInt()
+
+  val saldoBarraRef: String
+    get() {
+      return "${barcode ?: ""}   |   ${ref ?: ""}"
+    }
+
+  var marcadoConfProp: Boolean = false
+
+  fun marcadoConf(userNo: Int, data: LocalDate): Boolean {
+    this.marcadoConfProp = (estoqueUser == userNo) && (estoqueData.toSaciDate() == data.toSaciDate())
+    return this.marcadoConfProp
+  }
+
+  val estoqueDif: Int?
+    get() {
+      if (estoqueCD == null && estoqueLoja == null) {
+        return null
+      }
+
+      if (saldo == null) {
+        return null
+      }
+
+      val estLoja = estoqueLoja ?: 0
+      val estCD = estoqueCD ?: 0
+      val estSaldo = saldo ?: 0
+
+      return estLoja + estCD - estSaldo
+    }
+
+  val diferenca: Int
+    get() {
+      val estCD = kardec ?: 0
+      val estSis = saldo ?: 0
+      return estSis - estCD
+    }
+
+  fun dataInicialDefault(): LocalDate {
+    return dataInicial ?: LocalDate.now().withDayOfMonth(1)
+  }
+
+  val codigoStr
+    get() = this.codigo?.toString() ?: ""
+
+  val kardecEmb: Double?
+    get() {
+      return when {
+        descricao?.startsWith("SVS E-COLOR") == true -> {
+          if (kardec == null) null else (kardec ?: 0) / 900.0
+        }
+
+        descricao?.startsWith("VRC COLOR") == true   -> {
+          if (kardec == null) null else (kardec ?: 0) / 1000.0
+        }
+
+        else                                         -> {
+          if (kardec == null) null else (kardec ?: 0) / ((embalagem ?: 0) * 1.00)
+        }
+      }
+    }
+
+  fun updateConferencia() {
+    saci.updateProdutoConferencia(this)
+  }
+
+  fun updateAcerto() {
+    saci.updateProdutoAcerto(this)
+  }
+
+  fun updateMarca() {
+    saci.updateProdutoMarca(this)
+  }
+
+  fun updateLocalizacao() {
+    saci.updateProdutoLocalizacao(this)
+  }
+
+  fun updateKardec() {
+    saci.updateProdutoKardec(this)
+  }
+
+  fun recebimentos(loja: Int, dataInicial: LocalDate): List<ProdutoKardec> {
+    val filtro = FiltroNotaRecebimentoProduto(
+      loja = loja,
+      pesquisa = "",
+      marca = EMarcaRecebimento.RECEBIDO,
+      dataFinal = null,
+      dataInicial = dataInicial,
+      localizacao = listOf("TODOS"),
+      prdno = prdno ?: "",
+      grade = grade ?: "",
+      tipoNota = EListaContas.TODOS,
+    )
+    return saci.findNotaRecebimentoProduto(filtro).map { nota ->
+      ProdutoKardec(
+        loja = loja,
+        prdno = prdno ?: "",
+        grade = grade ?: "",
+        data = nota.data,
+        doc = nota.nfEntrada ?: "",
+        tipo = ETipoKardec.RECEBIMENTO,
+        vencimento = nota.vencimento,
+        qtde = nota.quant ?: 0,
+        saldo = 0,
+        userLogin = nota.login ?: "",
+        observacao = nota.observacaoNota
+      )
+    }
+  }
+
+  private fun filtroRessuprimento(marca: EMarcaRessuprimento, dataInicial: LocalDate): FiltroRessuprimento {
+    return FiltroRessuprimento(
+      numero = 0,
+      pesquisa = "",
+      dataNotaInicial = dataInicial,
+      dataPedidoInicial = dataInicial,
+      marca = marca,
+      temNota = ETemNota.TEM_NOTA,
+      lojaRessu = 0,
+      prdno = this.prdno ?: "",
+      grade = grade ?: "",
+    )
+  }
+
+  private fun ressuprimento(marca: EMarcaRessuprimento, dataInicial: LocalDate): List<ProdutoKardec> {
+    val filtro = filtroRessuprimento(marca, dataInicial)
+    val ressuprimentos = saci.findRessuprimento(filtro, userRessuprimentoLocais())
+    return ressuprimentos.flatMap { ressuprimento ->
+      val prdno = prdno ?: return emptyList()
+      val grade = grade ?: return emptyList()
+      val mult = when (marca) {
+        EMarcaRessuprimento.REC -> -1
+        EMarcaRessuprimento.ENT -> -1
+        else                    -> 0
+      }
+      ressuprimento.produtos(prdno, grade).map { produto ->
+        ProdutoKardec(
+          loja = ressuprimento.loja ?: 0,
+          prdno = prdno,
+          grade = grade,
+          data = ressuprimento.dataBaixa,
+          doc = ressuprimento.notaBaixa ?: "",
+          tipo = ETipoKardec.RESSUPRIMENTO,
+          qtde = (produto.qtQuantNF ?: 0) * mult,
+          saldo = 0,
+          userLogin = ressuprimento.entregueSPor ?: "",
+          observacao = ressuprimento.observacao
+        )
+      }
+    }
+  }
+
+  fun ressuprimento(dataInicial: LocalDate): List<ProdutoKardec> {
+    val listaKardec =
+        ressuprimento(EMarcaRessuprimento.ENT, dataInicial) + ressuprimento(EMarcaRessuprimento.REC, dataInicial)
+    return listaKardec.filter {
+      it.loja == 4
+    }
+  }
+
+  fun expedicao(loja: Int, dataInicial: LocalDate): List<ProdutoKardec> {
+    val filtro = FiltroNota(
+      marca = EMarcaNota.ENT,
+      tipoNota = ETipoNotaFiscal.TODOS,
+      loja = loja,
+      pesquisa = "",
+      prdno = prdno ?: "",
+      grade = grade ?: "",
+      dataInicial = dataInicial.minusDays(7),
+      dataEntregaInicial = null,
+      dataFinal = LocalDate.now(),
+      dataNotas = dataInicial.minusDays(7),
+      numero = 0,
+      localizacaoNota = listOf("TODOS"),
+    )
+    val notasEnt = saci.findNotaSaidaPrd(filtro = filtro.copy(marca = EMarcaNota.ENT))
+    val notas = notasEnt.filter {
+      it.cancelada != "S"
+    }
+    val ret = notas.flatMap { nota ->
+      val tipo = if (nota.tipoNotaSaida == ETipoNotaFiscal.ENTRE_FUT.name) {
+        ETipoKardec.ENTREGA
+      } else {
+        ETipoKardec.EXPEDICAO
+      }
+
+      val usuario = if (nota.tipoNotaSaida == ETipoNotaFiscal.ENTRE_FUT.name) {
+        (nota.usuarioCD ?: "").split("-").firstOrNull() ?: ""
+      } else {
+        (nota.usuarioExp ?: "").split("-").firstOrNull() ?: ""
+      }
+
+      //Validações
+      val data = nota.dataEntrega ?: nota.data ?: return@flatMap emptyList()
+      if (data < dataInicial) return@flatMap emptyList()
+
+      val produtosEnt =
+          nota.produtos(marca = EMarcaNota.ENT, prdno = prdno ?: "", grade = grade ?: "", todosLocais = true)
+
+      val nota1 = nota.notaEntrega ?: ""
+      val nota2: String = "${nota.numero}/${nota.serie}"
+
+      val notaFutura = when {
+        nota1.endsWith("/1") -> nota1
+        nota2.endsWith("/1") -> nota2
+        nota1.isNotEmpty()   -> nota1
+        nota2.isNotEmpty()   -> nota2
+        else                 -> ""
+      }
+
+      val notaEntrega = when {
+        nota1.endsWith("/3") -> nota1
+        nota2.endsWith("/3") -> nota2
+        nota1.isNotEmpty()   -> nota2
+        else                 -> ""
+      }
+
+      val listExp = produtosEnt.filter { produto ->
+        produto.gradeEfetiva == (grade ?: "")
+      }.map { produto ->
+        ProdutoKardec(
+          loja = loja,
+          prdno = prdno ?: "",
+          grade = produto.gradeEfetiva,
+          data = data,
+          doc = notaFutura,
+          nfEnt = notaEntrega,
+          tipo = tipo,
+          qtde = -(produto.quantidade ?: 0),
+          saldo = 0,
+          userLogin = nota.usuarioSingCD ?: usuario,
+          observacao = "${nota.observacao} loja ${nota.loja}"
+        )
+      }
+      listExp.distinctBy { "${it.loja} ${it.doc} ${it.nfEnt}" }
+    }
+    return ret
+  }
+
+  fun reposicao(loja: Int, dataInicial: LocalDate): List<ProdutoKardec> {
+    val localizacao = listOf("TODOS")
+    val filtro = FiltroReposicao(
+      loja = loja,
+      pesquisa = "",
+      marca = EMarcaReposicao.ENT,
+      localizacao = localizacao,
+      dataInicial = dataInicial,
+      dataFinal = null,
+      prdno = prdno ?: "",
+      grade = grade ?: "",
+      metodo = EMetodo.TODOS,
+    )
+    return saci.findResposicaoProduto(filtro).mapNotNull { produto ->
+      if (produto.marca != EMarcaReposicao.ENT.num) return@mapNotNull null
+
+      val tipo = when (produto.metodo) {
+        431  -> ETipoKardec.REPOSICAO
+        432  -> ETipoKardec.RETORNO
+        433  -> ETipoKardec.ACERTO
+        else -> return@mapNotNull null
+      }
+
+      val mult = when (produto.metodo) {
+        431  -> -1
+        432  -> 1
+        433  -> produto.multAcerto ?: 0
+        else -> return@mapNotNull null
+      }
+
+      ProdutoKardec(
+        loja = produto.loja ?: 0,
+        prdno = produto.prdno ?: "",
+        grade = produto.grade ?: "",
+        data = produto.data,
+        doc = produto.numero.toString(),
+        tipo = tipo,
+        qtde = mult * (produto.quantidade ?: 0),
+        saldo = 0,
+        userLogin = produto.entregueSNome ?: "",
+        observacao = produto.observacao
+      )
+    }
+  }
+
+  fun saldoInicial(loja: Int, dataInicial: LocalDate): List<ProdutoKardec> {
+    if (qtConferencia == null) {
+      val list = saci.findSaldoData(
+        loja = loja,
+        codigo = codigo.toString(),
+        grade = grade ?: "",
+        dataInicial = dataInicial
+      )
+      return list.map { saldo ->
+        ProdutoKardec(
+          loja = saldo.storeno,
+          prdno = saldo.prdno,
+          grade = saldo.grade,
+          data = dataInicial,
+          doc = "Estoque",
+          tipo = ETipoKardec.INICIAL,
+          qtde = saldo.quant,
+          saldo = 0,
+          userLogin = "ADM",
+          observacao = ""
+        )
+      }
+    }
+
+    val produtoKardec = ProdutoKardec(
+      loja = loja,
+      prdno = prdno,
+      grade = grade ?: "",
+      data = dataInicial,
+      doc = "Estoque",
+      tipo = ETipoKardec.INICIAL,
+      qtde = qtConferencia,
+      saldo = 0,
+      userLogin = "ADM",
+      observacao = ""
+    )
+    return listOf(produtoKardec)
+  }
+
+  fun acertoEstoque(loja: Int, dataInicial: LocalDate): List<ProdutoKardec> {
+    val list = saci.findAcertoEstoque(
+      loja = loja,
+      codigo = codigo.toString(),
+      grade = grade ?: "",
+      dataInicial = dataInicial
+    )
+    return list.map { saldo ->
+      ProdutoKardec(
+        loja = saldo.loja,
+        prdno = saldo.prdno,
+        grade = saldo.grade,
+        data = saldo.data,
+        doc = saldo.pedido.toString(),
+        tipo = ETipoKardec.ACERTO_ESTOQUE,
+        qtde = saldo.quantidade,
+        saldo = 0,
+        userLogin = "ADM",
+        observacao = saldo.observacao
+      )
+    }
+  }
+
+  fun limpaAcerto() {
+    saci.removeAcertoProduto(this)
+  }
+
+  companion object {
+    fun findProdutoEstoque(filter: FiltroProdutoEstoque): List<ProdutoEstoque> {
+      return saci.findProdutoEstoque(filter)
+    }
+  }
+}
+
+data class FiltroProdutoEstoque(
+  val loja: Int = 0,
+  val pesquisa: String,
+  val codigo: Int,
+  val grade: String,
+  val caracter: ECaracter,
+  val localizacao: String?,
+  val fornecedor: String,
+  val centroLucro: Int = 0,
+  val pedido: Int = 0,
+  val estoque: EEstoque = EEstoque.TODOS,
+  val saldo: Int = 0,
+  val inativo: EInativo,
+  val uso: EUso = EUso.TODOS,
+  val listaUser: List<String>,
+  val letraDup: ELetraDup = ELetraDup.TODOS,
+) {
+  val prdno = if (codigo == 0) "" else codigo.toString().lpad(16, " ")
+}
---
+++ src/main/resources/sqlSaci/findProdutoControle.sql
@@ -0,0 +1,264 @@
+USE sqldados;
+
+SET SQL_MODE = '';
+
+DO @PESQUISA := TRIM(:pesquisa);
+DO @PESQUISANUM := IF(@PESQUISA REGEXP '[0-9]+', @PESQUISA, '');
+DO @PESQUISASTART := CONCAT(@PESQUISA, '%');
+DO @PESQUISALIKE := CONCAT('%', @PESQUISA, '%');
+
+DO @FORNECEDOR_NUMERO := IF(:fornecedor REGEXP '^[0-9]+$', :fornecedor, 0);
+DO @FORNECEDOR_NOME := IF(:fornecedor REGEXP '^[0-9]+$', '', :fornecedor);
+
+DROP TEMPORARY TABLE IF EXISTS T_PRD;
+CREATE TEMPORARY TABLE T_PRD
+(
+  PRIMARY KEY (prdno)
+)
+SELECT no AS prdno, mfno, mfno_ref, name, typeno, clno, qttyPackClosed
+FROM
+  sqldados.prd AS P
+WHERE (((P.dereg & POW(2, 2) = 0) AND (:inativo = 'N')) OR
+       ((P.dereg & POW(2, 2) != 0) AND (:inativo = 'S')) OR
+       (:inativo = 'T'))
+  AND (((P.bits & POW(2, 13) = 0) AND (:uso = 'N')) OR
+       ((P.bits & POW(2, 13) != 0) AND (:uso = 'S')) OR
+       (:uso = 'T'))
+  AND (P.groupno = :centroLucro OR P.deptno = :centroLucro OR P.clno = :centroLucro OR :centroLucro = 0)
+  AND ((:caracter = 'S' AND P.name NOT REGEXP '^[A-Z0-9]') OR (:caracter = 'N' AND P.name REGEXP '^[A-Z0-9]') OR
+       (:caracter = 'T'))
+  AND (P.no = :prdno OR :prdno = '')
+  AND CASE :letraDup
+        WHEN 'S' THEN (SUBSTRING_INDEX(P.name, ' ', 2) REGEXP
+                       '^..(AA|BB|CC|DD|EE|FF|GG|HH|II|JJ|KK|LL|MM|NN|OO|PP|QQ|RR|SS|TT|UU|VV|WW|XX|YY|ZZ)' OR
+                       P.name LIKE '3MM%') = TRUE
+        WHEN 'N' THEN (SUBSTRING_INDEX(P.name, ' ', 2) REGEXP
+                       '^..(AA|BB|CC|DD|EE|FF|GG|HH|II|JJ|KK|LL|MM|NN|OO|PP|QQ|RR|SS|TT|UU|VV|WW|XX|YY|ZZ)' OR
+                       P.name LIKE '3MM%') = FALSE
+        WHEN 'T' THEN TRUE
+                 ELSE FALSE
+      END;
+
+DROP TEMPORARY TABLE IF EXISTS T_LOC_NERUS;
+CREATE TEMPORARY TABLE T_LOC_NERUS
+(
+  PRIMARY KEY (prdno, grade)
+)
+SELECT prdno       AS prdno,
+       grade       AS grade,
+       localizacao AS locNerus
+FROM
+  sqldados.prdloc
+WHERE storeno IN (2, 3, 4, 5, 8)
+  AND (storeno = :loja OR :loja = 0)
+  AND (prdno = :prdno OR :prdno = '')
+GROUP BY prdno, grade;
+
+DROP TEMPORARY TABLE IF EXISTS T_LOC_APP;
+CREATE TEMPORARY TABLE T_LOC_APP
+(
+  PRIMARY KEY (prdno, grade)
+)
+SELECT prdno                                                     AS prdno,
+       grade                                                     AS grade,
+       localizacao                                               AS locApp,
+       dataInicial                                               AS dataInicial,
+       IF(dataUpdate * 1 = 0, NULL, dataUpdate)                  AS dataUpdate,
+       kardec                                                    AS kardec,
+       IF(dataObservacao * 1 = 0, CURDATE() * 1, dataObservacao) AS dataObservacao,
+       qtConferencia                                             AS qtConferencia,
+       qtConfEdit                                                AS qtConfEdit,
+       qtConfEditLoja                                            AS qtConfEditLoja,
+       estoqueConfCD                                             AS estoqueConfCD,
+       estoqueConfLoja                                           AS estoqueConfLoja,
+       estoque                                                   AS estoque,
+       estoqueData                                               AS estoqueData,
+       estoqueCD                                                 AS estoqueCD,
+       estoqueLoja                                               AS estoqueLoja,
+       estoqueUser                                               AS estoqueUser
+FROM
+  sqldados.prdAdicional
+WHERE (storeno IN (2, 3, 4, 5, 8))
+  AND (storeno = IF(:loja = 0, 4, :loja))
+  AND (prdno = :prdno OR :prdno = '')
+GROUP BY prdno, grade;
+
+DROP TEMPORARY TABLE IF EXISTS T_BARCODE;
+CREATE TEMPORARY TABLE T_BARCODE
+(
+  PRIMARY KEY (prdno, grade)
+)
+SELECT P.no                                                                  AS prdno,
+       IFNULL(B.grade, '')                                                   AS grade,
+       MAX(TRIM(IF(B.grade IS NULL, IFNULL(P2.gtin, P.barcode), B.barcode))) AS codbar
+FROM
+  sqldados.prd                AS P
+    LEFT JOIN sqldados.prd2   AS P2
+              ON P.no = P2.prdno
+    LEFT JOIN sqldados.prdbar AS B
+              ON P.no = B.prdno AND B.grade != ''
+GROUP BY P.no, B.grade
+HAVING codbar != '';
+
+DROP TEMPORARY TABLE IF EXISTS T_ULT_ACERTO;
+CREATE TEMPORARY TABLE T_ULT_ACERTO
+(
+  PRIMARY KEY (numloja, prdno, grade)
+)
+SELECT numloja, prdno, grade, MAX(numero) AS numero
+FROM
+  sqldados.produtoEstoqueAcerto A
+GROUP BY numloja, prdno, grade;
+
+DROP TEMPORARY TABLE IF EXISTS T_ACERTO;
+CREATE TEMPORARY TABLE T_ACERTO
+(
+  PRIMARY KEY (numloja, prdno, grade)
+)
+SELECT A.numero,
+       A.numloja,
+       A.data,
+       A.hora,
+       A.usuario,
+       A.prdno,
+       A.grade,
+       A.estoqueSis,
+       A.estoqueLoja,
+       A.estoqueCD,
+       A.processado,
+       A.transacao,
+       A.login
+FROM
+  sqldados.produtoEstoqueAcerto A
+    INNER JOIN T_ULT_ACERTO
+               USING (numloja, prdno, grade, numero);
+
+DROP TEMPORARY TABLE IF EXISTS temp_pesquisa;
+CREATE TEMPORARY TABLE temp_pesquisa
+SELECT S.no                                                                           AS loja,
+       S.sname                                                                        AS lojaSigla,
+       E.prdno                                                                        AS prdno,
+       TRIM(PD.prdno) * 1                                                             AS codigo,
+       TRIM(MID(PD.name, 1, 37))                                                      AS descricao,
+       TRIM(MID(PD.name, 38, 3))                                                      AS unidade,
+       PD.typeno                                                                      AS tipo,
+       PD.clno                                                                        AS cl,
+       E.grade                                                                        AS grade,
+       ROUND(PD.qttyPackClosed / 1000)                                                AS embalagem,
+       SUM(CASE
+             WHEN PD.name LIKE 'SVS E-COLOR%' THEN TRUNCATE(
+                 ROUND((E.qtty_atacado + E.qtty_varejo) / 1000) / 900, 2)
+             WHEN PD.name LIKE 'VRC COLOR%'   THEN TRUNCATE(
+                 ROUND((E.qtty_atacado + E.qtty_varejo) / 1000) / 1000, 2)
+                                              ELSE TRUNCATE(
+                                                  ROUND((E.qtty_atacado + E.qtty_varejo) / 1000) /
+                                                  (PD.qttyPackClosed / 1000), 0)
+           END)                                                                       AS qtdEmbalagem,
+       IFNULL(A.estoque, 0)                                                           AS estoque,
+       LN.locNerus                                                                    AS locNerus,
+       IFNULL(A.locApp, '')                                                           AS locApp,
+       V.no                                                                           AS codForn,
+       V.name                                                                         AS fornecedor,
+       V.sname                                                                        AS fornecedorAbrev,
+       V.cgc                                                                          AS cnpjFornecedor,
+       ROUND(SUM(E.qtty_atacado + E.qtty_varejo) / 1000)                              AS saldo,
+       ROUND(SUM(E.qtty_atacado + E.qtty_varejo) / 1000) * (E.cm_real / 10000)        AS valorEstoque,
+       ROUND(SUM(E.qtty_varejo) / 1000)                                               AS saldoVarejo,
+       ROUND(SUM(E.qtty_atacado) / 1000)                                              AS saldoAtacado,
+       CAST(IF(IFNULL(A.dataInicial, 0) = 0, NULL, IFNULL(A.dataInicial, 0)) AS DATE) AS dataInicial,
+       A.dataUpdate                                                                   AS dataUpdate,
+       A.kardec                                                                       AS kardec,
+       A.estoqueConfCD                                                                AS estoqueConfCD,
+       A.estoqueConfLoja                                                              AS estoqueConfLoja,
+       CAST(A.dataObservacao AS DATE)                                                 AS dataConferencia,
+       qtConferencia                                                                  AS qtConferencia,
+       qtConfEdit                                                                     AS qtConfEdit,
+       qtConfEditLoja                                                                 AS qtConfEditLoja,
+       PC.refprice / 100                                                              AS preco,
+       A.estoqueUser                                                                  AS estoqueUser,
+       U.login                                                                        AS estoqueLogin,
+       A.estoqueData                                                                  AS estoqueData,
+       AC.estoqueCD                                                                   AS estoqueCD,
+       AC.estoqueLoja                                                                 AS estoqueLoja,
+       B.codbar                                                                       AS barcode,
+       PD.mfno_ref                                                                    AS ref,
+       AC.numero                                                                      AS numeroAcerto,
+       AC.processado                                                                  AS processado
+FROM
+  sqldados.stk                AS E
+    INNER JOIN sqldados.store AS S
+               ON E.storeno = S.no
+    INNER JOIN T_PRD          AS PD
+               USING (prdno)
+    LEFT JOIN  sqldados.vend  AS V
+               ON V.no = PD.mfno
+    LEFT JOIN  T_LOC_APP      AS A
+               USING (prdno, grade)
+    LEFT JOIN  T_LOC_NERUS    AS LN
+               USING (prdno, grade)
+    LEFT JOIN  sqldados.users AS U
+               ON U.no = A.estoqueUser
+    LEFT JOIN  T_BARCODE      AS B
+               USING (prdno, grade)
+    LEFT JOIN  sqldados.prp   AS PC
+               ON PC.storeno = 10 AND PC.prdno = E.prdno
+    LEFT JOIN  T_ACERTO       AS AC
+               ON E.storeno = AC.numloja AND E.prdno = AC.prdno AND E.grade = AC.grade
+WHERE (E.storeno = :loja OR :loja = 0)
+  AND (PD.mfno = @FORNECEDOR_NUMERO OR @FORNECEDOR_NUMERO = 0)
+  AND (V.name LIKE CONCAT('%', @FORNECEDOR_NOME, '%') OR @FORNECEDOR_NOME = '')
+GROUP BY E.prdno, E.grade
+HAVING (:estoque = '>' AND saldo > :saldo)
+    OR (:estoque = '<' AND saldo < :saldo)
+    OR (:estoque = '=' AND saldo = :saldo)
+    OR (:estoque = 'T');
+
+SELECT loja,
+       lojaSigla,
+       prdno,
+       codigo,
+       descricao,
+       unidade,
+       grade,
+       tipo,
+       cl,
+       embalagem,
+       qtdEmbalagem,
+       estoque,
+       locNerus,
+       locApp,
+       codForn,
+       fornecedor,
+       fornecedorAbrev,
+       cnpjFornecedor,
+       saldo,
+       valorEstoque,
+       saldoVarejo,
+       saldoAtacado,
+       dataInicial,
+       dataUpdate,
+       kardec,
+       IFNULL(dataConferencia, CURDATE()) AS dataConferencia,
+       qtConferencia,
+       qtConfEdit,
+       qtConfEditLoja,
+       preco,
+       estoqueUser,
+       estoqueLogin,
+       estoqueData,
+       estoqueCD,
+       estoqueLoja,
+       barcode,
+       ref,
+       numeroAcerto,
+       processado,
+       estoqueConfCD,
+       estoqueConfLoja
+FROM
+  temp_pesquisa
+WHERE (@PESQUISA = '' OR codigo = @PESQUISANUM OR descricao LIKE @PESQUISALIKE OR unidade LIKE @PESQUISA OR
+       (DATE_FORMAT(estoqueData, '%d/%m/%Y') LIKE @PESQUISALIKE))
+  AND (grade LIKE CONCAT(:grade, '%') OR :grade = '')
+  AND (locApp LIKE CONCAT(:localizacao, '%') OR :localizacao = '')
+  AND (MID(locApp, 1, 4) IN (:localizacaoUser) OR 'TODOS' IN (:localizacaoUser) OR TRIM(IFNULL(locApp, '')) = '')
+  AND (numeroAcerto = :pedido OR :pedido = 0)
