7dfab8629f900231f18ae11f8b4847a3fb037b78
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2026-01-24 14:09:24 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/view/recebimento/DlgAgendamento.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/recebimento/DlgAgendamento.kt
@@ -6,13 +6,14 @@ import br.com.astrosoft.produto.model.beans.Agenda
 import br.com.astrosoft.produto.model.beans.AgendaUpdate
 import br.com.astrosoft.produto.viewmodel.recebimento.TabAgendaViewModel
 import com.github.mvysny.karibudsl.v10.*
-import com.vaadin.flow.component.HasComponents
 import com.vaadin.flow.component.button.ButtonVariant.LUMO_PRIMARY
+import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.data.binder.Binder
 import java.time.Duration
 
 class DlgAgendamento(val viewModel: TabAgendaViewModel) : VerticalLayout() {
+  private lateinit var form: SubWindowForm
   private val binder = Binder(AgendaUpdate::class.java)
 
   init {
@@ -51,29 +52,40 @@ class DlgAgendamento(val viewModel: TabAgendaViewModel) : VerticalLayout() {
         bind(binder).bind(AgendaUpdate::recebedor)
       }
     }
+    hr()
+    horizontalLayout {
+      setWidthFull()
+      this.content {
+        this.align(right, baseline)
+      }
+      this.button("Fechar") {
+        icon = VaadinIcon.CLOSE.create()
+        onClick {
+          form.onClose(form)
+          form.close()
+        }
+      }
+
+      this.button("Salvar") {
+        this.addThemeVariants(LUMO_PRIMARY)
+        onClick {
+          viewModel.salvaAgendamento(binder.bean)
+          form.close()
+        }
+      }
+    }
   }
 
   fun edtAgendamento(agenda: Agenda) {
-    val form = SubWindowForm(
+    form = SubWindowForm(
       title = "Nr. Ordem ${agenda.invno}  NF ${agenda.nf}",
-      toolBar = ::toolBar,
-      fullSize = false
+      toolBar = { },
+      fullSize = false,
+      hasButtonClose = false
     ) {
       binder.bean = agenda.agendaUpdate()
       this
     }
     form.open()
-  }
-
-  private fun toolBar(hasComponents: HasComponents, subWindowForm: SubWindowForm) {
-    hasComponents.apply {
-      this.button("Salvar") {
-        this.addThemeVariants(LUMO_PRIMARY)
-        onClick {
-          viewModel.salvaAgendamento(binder.bean)
-          subWindowForm.close()
-        }
-      }
-    }
   }
 }
