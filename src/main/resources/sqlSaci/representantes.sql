1cef3011dd17edde8002b6b14a6ccf5825c6464d
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2026-02-07 09:42:34 -0300

    Formulários para os representantes

--- src/main/resources/sqlSaci/representantes.sql
+++ src/main/resources/sqlSaci/representantes.sql
@@ -1,6 +1,6 @@
 SELECT RV.vendno                                                                    AS vendno,
        CAST(CONCAT(R.no, ' ', R.name) AS CHAR)                                      AS nome,
-       CAST(CONCAT(TRIM(MID(R.ddd, 1, 5)), ' ', TRIM(MID(R.phone, 1, 10))) AS CHAR) AS telefone,
+       CAST(CONCAT(TRIM(MID(R.ddd, 1, 5)), ' - ', TRIM(MID(R.phone, 1, 10))) AS CHAR) AS telefone,
        IF(R.celular = 0, '', CAST(R.celular AS CHAR))                               AS celular,
        R.email                                                                      AS email
 FROM
