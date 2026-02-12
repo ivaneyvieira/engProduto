ecd1842b745026ed172f0217e9c2f3cd63f13178
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2026-02-12 11:41:52 -0300

    Botão para altera a senha

--- src/main/kotlin/br/com/astrosoft/framework/view/layout/AppLayoutAbstract.kt
+++ src/main/kotlin/br/com/astrosoft/framework/view/layout/AppLayoutAbstract.kt
@@ -3,6 +3,7 @@ package br.com.astrosoft.framework.view.layout
 import br.com.astrosoft.framework.model.config.AppConfig
 import br.com.astrosoft.framework.model.security.LoginService
 import br.com.astrosoft.framework.view.config.ViewUtil
+import br.com.astrosoft.framework.view.vaadin.helper.DlgAlteraSenha
 import com.github.mvysny.karibudsl.v10.*
 import com.vaadin.flow.component.HasComponents
 import com.vaadin.flow.component.applayout.AppLayout
@@ -10,15 +11,28 @@ import com.vaadin.flow.component.applayout.AppLayout
 abstract class AppLayoutAbstract : AppLayout() {
   init {
     isDrawerOpened = false
-    navbar {
+    navbar(true) {
       drawerToggle()
       h3(AppConfig.title)
       horizontalLayout {
         isExpand = true
+        this.isMargin = false
+        this.isPadding = false
       }
-      button("Sair") {
-        onClick {
-          LoginService.get().logout()
+      horizontalLayout {
+        this.isMargin = true
+        this.isPadding = false
+        this.isSpacing = true
+
+        button("Senha") {
+          onClick {
+            DlgAlteraSenha().show()
+          }
+        }
+        button("Sair") {
+          onClick {
+            LoginService.get().logout()
+          }
         }
       }
     }
---
+++ src/main/kotlin/br/com/astrosoft/framework/view/vaadin/helper/DlgAlteraSenha.kt
@@ -0,0 +1,39 @@
+package br.com.astrosoft.framework.view.vaadin.helper
+
+import br.com.astrosoft.framework.model.config.AppConfig
+import br.com.astrosoft.produto.model.beans.UserSaci
+import com.github.mvysny.karibudsl.v10.passwordField
+import com.vaadin.flow.component.formlayout.FormLayout
+
+class DlgAlteraSenha {
+  fun show() {
+    val user = AppConfig.userLogin() as? UserSaci ?: return
+    val form = FormLayout()
+    val edtSenhaAtual = form.passwordField("Senha Atual")
+    val edtNovaSenha = form.passwordField("Nova Senha")
+    val edtConfirmaSenha = form.passwordField("Confirma Nova Senha")
+
+    DialogHelper.showForm("Alterar Senha", form) {
+      val senhaAtual = edtSenhaAtual.value ?: ""
+      val novaSenha = edtNovaSenha.value ?: ""
+      val confirmaSenha = edtConfirmaSenha.value ?: ""
+
+      when {
+        user.senha.uppercase().trim() != senhaAtual.uppercase().trim() -> {
+          DialogHelper.showError("Senha atual incorreta")
+        }
+        novaSenha.isBlank() -> {
+          DialogHelper.showError("Nova senha não pode ser vazia")
+        }
+        novaSenha != confirmaSenha -> {
+          DialogHelper.showError("A nova senha e a confirmação não coincidem")
+        }
+        else -> {
+          user.senha = novaSenha
+          UserSaci.updateUser(user)
+          DialogHelper.showInformation("Senha alterada com sucesso")
+        }
+      }
+    }
+  }
+}
