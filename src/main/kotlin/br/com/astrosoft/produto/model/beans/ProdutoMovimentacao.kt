cddacca3a7fb992029deb4c720b3575469aace5d
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2026-03-06 14:49:40 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/model/beans/ProdutoMovimentacao.kt
+++ src/main/kotlin/br/com/astrosoft/produto/model/beans/ProdutoMovimentacao.kt
@@ -230,10 +230,10 @@ enum class ERota(val numero: Int, val descricao: String) {
   CD_LJ(0, "CD-LJ"),
   LJ_CD(1, "LJ-CD"),
   //Rota42, Rota43, Rota45, Rota48
-  ROTA42(42, "Rota42"),
-  ROTA43(43, "Rota43"),
-  ROTA45(45, "Rota45"),
-  ROTA48(48, "Rota48"),
+  //ROTA42(42, "Rota42"),
+  //ROTA43(43, "Rota43"),
+  //ROTA45(45, "Rota45"),
+  //ROTA48(48, "Rota48"),
 }
 
 enum class EStatusMovimentacao(val codigo: String, val descricao: String) {
