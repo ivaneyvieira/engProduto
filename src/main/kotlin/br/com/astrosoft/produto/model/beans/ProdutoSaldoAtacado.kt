05c8f65153ac337983aa44a65614103139f987d7
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2026-03-11 11:00:54 -0300

    Copia findProdutoSaldoAtacado

---
+++ src/main/kotlin/br/com/astrosoft/produto/model/beans/ProdutoSaldoAtacado.kt
@@ -0,0 +1,95 @@
+package br.com.astrosoft.produto.model.beans
+
+import br.com.astrosoft.produto.model.saci
+
+class ProdutoSaldo(
+  var loja: Int?,
+  var prdno: String?,
+  var codigo: Int?,
+  var descricao: String?,
+  var gradeProduto: String?,
+  var unidade: String?,
+  var tipoValidade: String?,
+  var mesesGarantia: Int?,
+  var estoqueLojas: Int?,
+  var estoqueLojasVarejo: Int?,
+  var estoqueLojasAtacado: Int?,
+  var qttyVarejo: Int?,
+  var qttyAtacado: Int?,
+  var qttyTotal: Int?,
+  var custoAtacado: Double?,
+  var custoLojasAtacado: Double?,
+  var tributacao: String?,
+  var rotulo: String?,
+  var ncm: String?,
+  var fornecedor: Int?,
+  var abrev: String?,
+  var tipo: Int?,
+  var cl: Int?,
+  var localizacao: String?,
+  var prdnoRel: String?,
+  var codigoRel: Int?
+) {
+  val codigoStr
+    get() = this.codigo?.toString() ?: ""
+
+  companion object {
+    fun findProdutoSaldo(filtro: FiltroProdutoSaldo): List<ProdutoSaldo> {
+      return saci.findProdutoSaldo(filtro)
+    }
+  }
+}
+
+data class FiltroProdutoSaldo(
+  val loja: Int,
+  val pesquisa: String,
+  val fornecedor: Int,
+  val tributacao: String,
+  val rotulo: String,
+  val tipo: Int,
+  val cl: Int,
+  val caracter: ECaracter,
+  val letraDup: ELetraDup,
+  val grade: Boolean,
+  val tipoSaldo: ETipoSaldo,
+  val estoque: EEstoque,
+  val saldo: Int,
+  val consumo: EConsumo,
+  val update: Boolean,
+) {
+  fun lojaSigla(): String {
+    return saci.allLojas().firstOrNull { it.no == loja }?.sname ?: ""
+  }
+}
+
+enum class ECaracter(val value: String, val descricao: String) {
+  SIM("S", "Sim"),
+  NAO("N", "Não"),
+  TODOS("T", "Todos"),
+}
+
+enum class EConsumo(val value: String, val descricao: String) {
+  SIM("S", "Sim"),
+  NAO("N", "Não"),
+  TODOS("T", "Todos"),
+}
+
+enum class ELetraDup(val value: String, val descricao: String) {
+  SIM("S", "Sim"),
+  NAO("N", "Não"),
+  TODOS("T", "Todos"),
+}
+
+enum class EEstoque(val value: String, val descricao: String) {
+  MENOR("<", "<"),
+  MAIOR(">", ">"),
+  IGUAL("=", "="),
+  TODOS("T", "Todos"),
+  ENTRE("E", "Entre"),
+}
+
+enum class ETipoSaldo(val descricao: String) {
+  VAREJO("Varejo"),
+  ATACADO("Atacado"),
+  TOTAL("Total")
+}
