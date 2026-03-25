ae6bb434d89e12242cfbdce2b3685b0d566dd7ff
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2026-03-25 01:30:31 -0300

    Copia painel de usuários

---
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/vendaRef/VendaRefUsrViewModel.kt
@@ -0,0 +1,29 @@
+package br.com.astrosoft.produto.viewmodel.vendaRef
+
+import br.com.astrosoft.framework.viewmodel.ITabUser
+import br.com.astrosoft.framework.viewmodel.TabUsrViewModel
+import br.com.astrosoft.produto.model.beans.UserSaci
+import br.com.astrosoft.produto.viewmodel.retira.PedidoRetiraViewModel
+
+class PedidoRetiraUsrViewModel(val viewModel: PedidoRetiraViewModel) : TabUsrViewModel(viewModel) {
+  override val subView
+    get() = viewModel.view.tabRetiraUsr
+
+  override fun UserSaci.desative() {
+    this.pedidoRetira = false
+  }
+
+  override fun UserSaci.isActive(): Boolean {
+    return this.pedidoRetira
+  }
+
+  override fun UserSaci.update(usuario: UserSaci) {
+    this.retiraImprimir = usuario.retiraImprimir
+    this.retiraImpresso = usuario.retiraImpresso
+    this.lojaRetira = usuario.lojaRetira
+    this.impressoraRet = usuario.impressoraRet
+    this.retiraTipo = usuario.retiraTipo
+  }
+}
+
+interface ITabPedidoRetiraUsr : ITabUser
