b6a7eb19e4bc569b51d2dba8773406e31740781c
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2026-03-28 09:34:37 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/view/vendaRef/TabResumoPgto.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/vendaRef/TabResumoPgto.kt
@@ -21,19 +21,28 @@ import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.select.Select
+import com.vaadin.flow.component.textfield.IntegerField
 import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.component.textfield.TextFieldVariant
 import com.vaadin.flow.data.value.ValueChangeMode
+import org.vaadin.addons.componentfactory.monthpicker.MonthPicker
 import java.time.LocalDate
+import java.time.YearMonth
 
 class TabResumoPgto(val viewModel: TabResumoPgtoViewModel) :
   TabPanelGrid<NotaResumoPgto>(NotaResumoPgto::class), ITabResumoPgto {
   private lateinit var cmbLoja: Select<Loja>
   private lateinit var chkLoja: Checkbox
   private lateinit var cmbData: Select<AgrupaData>
+
   private lateinit var chkParcela: Checkbox
   private lateinit var edtPesquisa: TextField
   private lateinit var edtDataInicial: DatePicker
   private lateinit var edtDataFinal: DatePicker
+  private lateinit var cmbMesInicial: MonthPicker
+  private lateinit var cmbMesFinal: MonthPicker
+  private lateinit var cmbAnoInicial: IntegerField
+  private lateinit var cmbAnoFinal: IntegerField
 
   fun init() {
     cmbLoja.setItems(viewModel.findAllLojas() + listOf(Loja.lojaZero))
@@ -105,6 +114,44 @@ class TabResumoPgto(val viewModel: TabResumoPgtoViewModel) :
                   AgrupaData.ANO -> "Ano"
                 }
               )
+
+              when (it.value) {
+                AgrupaData.DIA -> {
+                  edtDataInicial.isVisible = true
+                  edtDataFinal.isVisible = true
+                  cmbMesInicial.isVisible = false
+                  cmbMesFinal.isVisible = false
+                  cmbAnoInicial.isVisible = false
+                  cmbAnoFinal.isVisible = false
+                }
+
+                AgrupaData.MES -> {
+                  edtDataInicial.isVisible = false
+                  edtDataFinal.isVisible = false
+                  cmbMesInicial.isVisible = true
+                  cmbMesFinal.isVisible = true
+                  cmbAnoInicial.isVisible = false
+                  cmbAnoFinal.isVisible = false
+                  val dataI = edtDataInicial.value ?: LocalDate.now()
+                  val dataF = edtDataFinal.value ?: LocalDate.now()
+                  cmbMesInicial.value = YearMonth.of(dataI.year, dataI.monthValue)
+                  cmbMesFinal.value = YearMonth.of(dataF.year, dataF.monthValue)
+                }
+
+                AgrupaData.ANO -> {
+                  edtDataInicial.isVisible = false
+                  edtDataFinal.isVisible = false
+                  cmbMesInicial.isVisible = false
+                  cmbMesFinal.isVisible = false
+                  cmbAnoInicial.isVisible = true
+                  cmbAnoFinal.isVisible = true
+                  val dataI = edtDataInicial.value ?: LocalDate.now()
+                  val dataF = edtDataFinal.value ?: LocalDate.now()
+                  cmbAnoInicial.value = dataI.year
+                  cmbAnoFinal.value = dataF.year
+                }
+              }
+
               viewModel.updateView()
             }
           }
@@ -117,12 +164,13 @@ class TabResumoPgto(val viewModel: TabResumoPgtoViewModel) :
         this.isSpacing = true
 
         edtPesquisa = textField("Pesquisa") {
-          this.width = "300px"
-          valueChangeMode = ValueChangeMode.TIMEOUT
+          this.width = "150px"
+          valueChangeMode = ValueChangeMode.LAZY
           addValueChangeListener {
             viewModel.updateView()
           }
         }
+
         edtDataInicial = datePicker("Data inicial") {
           this.localePtBr()
           this.value = LocalDate.now()
@@ -137,6 +185,134 @@ class TabResumoPgto(val viewModel: TabResumoPgtoViewModel) :
             viewModel.updateView()
           }
         }
+
+        cmbMesInicial = MonthPicker().apply {
+          this.isVisible = false
+          this.label = "Mês Inicial"
+          this.seti18n(
+            MonthPicker.MonthPickerI18n()
+              .setMonthNames(
+                listOf(
+                  "Janeiro",
+                  "Fevereiro",
+                  "Março",
+                  "Abril",
+                  "Maio",
+                  "Junho",
+                  "Julho",
+                  "Agosto",
+                  "Setembro",
+                  "Outubro",
+                  "Novembro",
+                  "Dezembro"
+                )
+              )
+              .setMonthLabels(
+                listOf(
+                  "Jan",
+                  "Fev",
+                  "Mar",
+                  "Abr",
+                  "Mai",
+                  "Jun",
+                  "Jul",
+                  "Ago",
+                  "Set",
+                  "Out",
+                  "Nov",
+                  "Dez"
+                )
+              )
+              .setFormat("MM/YYYY")
+          )
+          this.value = YearMonth.now()
+          addValueChangeListener {
+            if (it.isFromClient) {
+              viewModel.updateView()
+            }
+          }
+        }
+        cmbMesFinal = MonthPicker().apply {
+          this.isVisible = false
+          this.label = "Mês Final"
+          this.seti18n(
+            MonthPicker.MonthPickerI18n()
+              .setMonthNames(
+                listOf(
+                  "Janeiro",
+                  "Fevereiro",
+                  "Março",
+                  "Abril",
+                  "Maio",
+                  "Junho",
+                  "Julho",
+                  "Agosto",
+                  "Setembro",
+                  "Outubro",
+                  "Novembro",
+                  "Dezembro"
+                )
+              )
+              .setMonthLabels(
+                listOf(
+                  "Jan",
+                  "Fev",
+                  "Mar",
+                  "Abr",
+                  "Mai",
+                  "Jun",
+                  "Jul",
+                  "Ago",
+                  "Set",
+                  "Out",
+                  "Nov",
+                  "Dez"
+                )
+              )
+              .setFormat("MM/YYYY")
+          )
+          this.value = YearMonth.now()
+          addValueChangeListener {
+            if (it.isFromClient) {
+              viewModel.updateView()
+            }
+          }
+        }
+
+        add(cmbMesInicial, cmbMesFinal)
+
+        cmbAnoInicial = integerField("Ano Inicial") {
+          this.value = LocalDate.now().year
+          this.addThemeVariants(TextFieldVariant.LUMO_ALIGN_RIGHT)
+          this.isVisible = false
+          this.width = "5rem"
+          this.valueChangeMode = ValueChangeMode.LAZY
+          addValueChangeListener {
+            if (it.isFromClient) {
+              val value = it.value
+              if (value in 1900..<2100) {
+                viewModel.updateView()
+              }
+            }
+          }
+        }
+
+        cmbAnoFinal = integerField("Ano Final") {
+          this.value = LocalDate.now().year
+          this.addThemeVariants(TextFieldVariant.LUMO_ALIGN_RIGHT)
+          this.isVisible = false
+          this.width = "5rem"
+          this.valueChangeMode = ValueChangeMode.LAZY
+          addValueChangeListener {
+            if (it.isFromClient) {
+              val value = it.value
+              if (value in 1900..<2100) {
+                viewModel.updateView()
+              }
+            }
+          }
+        }
+
         button("Relatorio") {
           icon = VaadinIcon.PRINT.create()
           onClick {
@@ -191,14 +367,31 @@ class TabResumoPgto(val viewModel: TabResumoPgtoViewModel) :
   }
 
   override fun filtro(): FiltroNotaResumoPgto {
+    val grupo = cmbData.value ?: AgrupaData.DIA
+    val dataI = when (grupo) {
+      AgrupaData.DIA -> edtDataInicial.value ?: LocalDate.now()
+      AgrupaData.MES -> cmbMesInicial.value?.atDay(1) ?: LocalDate.now().withDayOfMonth(1)
+      AgrupaData.ANO -> LocalDate.of(cmbAnoInicial.value ?: LocalDate.now().year, 1, 1)
+    }
+    val dataF = when (grupo) {
+      AgrupaData.DIA -> edtDataFinal.value ?: LocalDate.now()
+      AgrupaData.MES -> cmbMesFinal.value?.atEndOfMonth() ?: LocalDate.now()
+        .withDayOfMonth(LocalDate.now().lengthOfMonth())
+
+      AgrupaData.ANO -> LocalDate.of(cmbAnoFinal.value ?: LocalDate.now().year, 12, 31)
+    }
+
+    edtDataInicial.value = dataI
+    edtDataFinal.value = dataF
+
     return FiltroNotaResumoPgto(
       loja = cmbLoja.value?.no ?: 0,
       agrupaLojas = chkLoja.value ?: false,
       agrupaParcelas = chkParcela.value ?: false,
-      agrupaDatas = cmbData.value ?: AgrupaData.DIA,
+      agrupaDatas = grupo,
       pesquisa = edtPesquisa.value ?: "",
-      dataInicial = edtDataInicial.value,
-      dataFinal = edtDataFinal.value,
+      dataInicial = dataI,
+      dataFinal = dataF,
     )
   }
 
