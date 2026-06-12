248769ceae3dcda92190bf9040bfe46b369a89cf
ivaneyvieira <ivaney@pinto.com.br>
2026-06-12 11:26:00 -0300

    WIP

--- src/main/kotlin/br/com/astrosoft/produto/view/devForReceb/DlgProdutosNotaPedido.kt
+++ src/main/kotlin/br/com/astrosoft/produto/view/devForReceb/DlgProdutosNotaPedido.kt
@@ -302,7 +302,7 @@ class DlgProdutosNotaPedido(val viewModel: TabNotaPedidoViewModel, var nota: Not
       columnGrid(NotaRecebimentoProdutoDev::totalGeralDevolucao, "Total", width = "90px")
 
       this.setPartNameGenerator {
-        if ((it.numAcerto ?: 0) > 0 && it.processado == true) {
+        if ((it.numAcerto ?: 0) > 0) {
           "amarelo"
         } else {
           null
