be1aa09bca46891958f6ee37b30b4aa5ae93246d
ivaneyvieira <ivaney@pinto.com.br>
2026-06-22 14:56:22 -0300

    WIP

--- src/main/resources/sqlSaci/findPrdCod.sql
+++ src/main/resources/sqlSaci/findPrdCod.sql
@@ -1,3 +1,26 @@
-SELECT no AS prdno, TRIM(no) AS codigo, mfno_ref AS ref
-FROM sqldados.prd
-WHERE no = LPAD(:cod, 16, ' ')
+USE sqldados;
+
+DROP TEMPORARY TABLE IF EXISTS T_REF_PRD;
+CREATE TEMPORARY TABLE T_REF_PRD SELECT 100 AS ordem, no AS prdno, TRIM(no) AS codigo, NULL AS grade, mfno_ref AS ref
+                                 FROM sqldados.prd
+                                 WHERE TRIM(no)  = :ref
+                                 UNION
+                                 SELECT DISTINCT 80             AS ordem,
+                                                 prdnoRef       AS prdno,
+                                                 TRIM(prdnoRef) AS codigo,
+                                                 grade,
+                                                 prdno          AS ref
+                                 FROM sqldados.mfprd
+                                 WHERE TRIM(prdnoRef) = :ref
+                                 UNION
+                                 SELECT DISTINCT 50          AS ordem,
+                                                 prdno       AS prdno,
+                                                 TRIM(prdno) AS codigo,
+                                                 grade,
+                                                 prdrefno    AS ref
+                                 FROM sqldados.prdrefpq
+                                 WHERE TRIM(prdno) = :ref;
+
+SELECT prdno, codigo, grade, ref
+FROM T_REF_PRD
+ORDER BY ordem
--- src/main/resources/sqlSaci/findPrdRef.sql
+++ src/main/resources/sqlSaci/findPrdRef.sql
@@ -1,3 +1,26 @@
-SELECT no AS prdno, TRIM(no) AS codigo, mfno_ref AS ref
-FROM sqldados.prd
-WHERE mfno_ref = :ref
+USE sqldados;
+
+DROP TEMPORARY TABLE IF EXISTS T_REF_PRD;
+CREATE TEMPORARY TABLE T_REF_PRD SELECT 100 AS ordem, no AS prdno, TRIM(no) AS codigo, NULL AS grade, mfno_ref AS ref
+                                 FROM sqldados.prd
+                                 WHERE mfno_ref = :ref
+                                 UNION
+                                 SELECT DISTINCT 80             AS ordem,
+                                                 prdnoRef       AS prdno,
+                                                 TRIM(prdnoRef) AS codigo,
+                                                 grade,
+                                                 prdno          AS ref
+                                 FROM sqldados.mfprd
+                                 WHERE TRIM(prdno) = :ref
+                                 UNION
+                                 SELECT DISTINCT 50          AS ordem,
+                                                 prdno       AS prdno,
+                                                 TRIM(prdno) AS codigo,
+                                                 grade,
+                                                 prdrefno    AS ref
+                                 FROM sqldados.prdrefpq
+                                 WHERE TRIM(prdrefno) = :ref;
+
+SELECT prdno, codigo, grade, ref
+FROM T_REF_PRD
+ORDER BY ordem
