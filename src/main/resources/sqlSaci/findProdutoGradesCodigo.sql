32fec2aa4a3ce6929fa85639d04ec5312e5db8e1
ivaneyvieira <ivaney@pinto.com.br>
2026-06-25 14:52:41 -0300

    WIP

--- src/main/resources/sqlSaci/findProdutoGradesCodigo.sql
+++ src/main/resources/sqlSaci/findProdutoGradesCodigo.sql
@@ -1,13 +1,13 @@
 SELECT TRIM(no)                             AS codigo,
        no                                   AS prdno,
        TRIM(MID(P.name, 1, 37))             AS descricao,
-       B.grade                              AS grade,
+       IFNULL(B.grade, '')                  AS grade,
        0                                    AS saldo,
        TRIM(COALESCE(B.barcode, P.barcode)) AS codigoBarras
 FROM
   sqldados.prd                AS P
     LEFT JOIN sqldados.prdbar AS B
               ON P.no = B.prdno AND TRIM(B.barcode) != ''
-WHERE  TRIM(P.no) = :codigo
+WHERE TRIM(P.no) = :codigo
 
 
