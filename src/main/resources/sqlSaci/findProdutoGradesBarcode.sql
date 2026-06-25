868c146608f742ed52269170f29eb51a2c774cb8
ivaneyvieira <ivaney@pinto.com.br>
2026-06-25 15:55:49 -0300

    wip

--- src/main/resources/sqlSaci/findProdutoGradesBarcode.sql
+++ src/main/resources/sqlSaci/findProdutoGradesBarcode.sql
@@ -1,14 +1,15 @@
-SELECT TRIM(no)                                      AS codigo,
-       no                                            AS prdno,
-       TRIM(MID(P.name, 1, 37))                      AS descricao,
-       IFNULL(B.grade, '')                           AS grade,
-       0                                             AS saldo,
-       TRIM(COALESCE(B.barcode, P2.gtin, P.barcode)) AS codigoBarras
+SELECT DISTINCT TRIM(no)                                      AS codigo,
+                no                                            AS prdno,
+                TRIM(MID(P.name, 1, 37))                      AS descricao,
+                IFNULL(B.grade, '')                           AS grade,
+                0                                             AS saldo,
+                TRIM(COALESCE(B.barcode, P2.gtin, P.barcode)) AS codigoBarras
 FROM
   sqldados.prd                AS P
     LEFT JOIN sqldados.prd2   AS P2
-              ON P.no = P2.prdno
+              ON P.no = P2.prdno AND LENGTH(TRIM(P2.gtin)) > 10
     LEFT JOIN sqldados.prdbar AS B
-              ON P.no = B.prdno AND TRIM(B.barcode) != ''
-WHERE TRIM(P.barcode) = :codigo OR TRIM(P2.gtin) = :codigo OR TRIM(B.barcode) = :codigo
-
+              ON P.no = B.prdno AND LENGTH(TRIM(B.barcode)) > 10
+WHERE P.barcode <=> LPAD(:codigo, 16, ' ')
+   OR P2.gtin <=> LPAD(:codigo, 16, ' ')
+   OR B.barcode <=> LPAD(:codigo, 16, ' ')
--- src/main/resources/sqlSaci/findProdutoGradesCodigo.sql
+++ src/main/resources/sqlSaci/findProdutoGradesCodigo.sql
@@ -1,13 +1,14 @@
-SELECT TRIM(no)                             AS codigo,
-       no                                   AS prdno,
-       TRIM(MID(P.name, 1, 37))             AS descricao,
-       IFNULL(B.grade, '')                  AS grade,
-       0                                    AS saldo,
-       TRIM(COALESCE(B.barcode, P.barcode)) AS codigoBarras
+SELECT DISTINCT TRIM(no)                                      AS codigo,
+                no                                            AS prdno,
+                TRIM(MID(P.name, 1, 37))                      AS descricao,
+                IFNULL(B.grade, '')                           AS grade,
+                0                                             AS saldo,
+                TRIM(COALESCE(B.barcode, P2.gtin, P.barcode)) AS codigoBarras
 FROM
   sqldados.prd                AS P
+    LEFT JOIN sqldados.prd2   AS P2
+              ON P.no = P2.prdno AND LENGTH(TRIM(P2.gtin)) > 10
     LEFT JOIN sqldados.prdbar AS B
-              ON P.no = B.prdno AND TRIM(B.barcode) != ''
-WHERE TRIM(P.no) = :codigo
-
+              ON P.no = B.prdno AND LENGTH(TRIM(B.barcode)) > 10
+WHERE P.no <=> LPAD(:codigo, 16, ' ')
 
