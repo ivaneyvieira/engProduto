6ade777424860e10e9f47fb6904a42fe09e15de6
ivaneyvieira <ivaney@pinto.com.br>
2026-07-13 05:13:35 -0300

    Ajuste na planilha do painel Contro CD

--- src/main/kotlin/br/com/astrosoft/produto/model/planilha/PlanilhaProdutoEstoque.kt
+++ src/main/kotlin/br/com/astrosoft/produto/model/planilha/PlanilhaProdutoEstoque.kt
@@ -1,6 +1,7 @@
 package br.com.astrosoft.produto.model.planilha
 
 import br.com.astrosoft.framework.model.planilha.Planilha
+import br.com.astrosoft.framework.view.vaadin.helper.columnGrid
 import br.com.astrosoft.produto.model.beans.ProdutoEstoque
 
 class PlanilhaProdutoEstoque : Planilha<ProdutoEstoque>("Estoque") {
@@ -8,14 +9,10 @@ class PlanilhaProdutoEstoque : Planilha<ProdutoEstoque>("Estoque") {
     columnSheet(ProdutoEstoque::codigo, header = "Código")
     columnSheet(ProdutoEstoque::descricao, header = "Descrição")
     columnSheet(ProdutoEstoque::grade, header = "Grade")
-    columnSheet(ProdutoEstoque::unidade, header = "UN")
 
-    columnSheet(ProdutoEstoque::qtdEmbalagem, header = "Estq Emb")
-    columnSheet(ProdutoEstoque::kardecEmb, header = "Emb CD")
-
-    columnSheet(ProdutoEstoque::saldo, header = "Estoque")
-    columnSheet(ProdutoEstoque::kardec, header = "Estq CD")
-
-    columnSheet(ProdutoEstoque::diferenca, header = "Diferença")
+    columnSheet(ProdutoEstoque::saldoSistema, header = "Sistema")
+    columnSheet(ProdutoEstoque::qtdDif, header = "Loja")
+    columnSheet(ProdutoEstoque::kardec, header = "CD")
+    columnSheet(ProdutoEstoque::quantDevolucao, header = "Gar")
   }
 }
