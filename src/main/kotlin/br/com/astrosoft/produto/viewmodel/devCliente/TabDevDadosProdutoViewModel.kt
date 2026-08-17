d94b3823c6ce9f579869ed3a046e13e8542d21ef
Ivaney Vieira de Sales <ivaney@pintos.com.br>
2026-08-17 20:50:02 -0300

    Copia painel de Produtos Devolvidos

---
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/devCliente/TabDevDadosProdutoViewModel.kt
@@ -0,0 +1,62 @@
+package br.com.astrosoft.produto.viewmodel.devCliente
+
+import br.com.astrosoft.framework.viewmodel.ITabView
+import br.com.astrosoft.framework.viewmodel.fail
+import br.com.astrosoft.produto.model.beans.EntradaDevCliProList
+import br.com.astrosoft.produto.model.beans.FiltroEntradaDevCliProList
+import br.com.astrosoft.produto.model.beans.Loja
+import br.com.astrosoft.produto.model.printText.ProdutosDevolucao
+
+class TabDevCliProdutoViewModel(val viewModel: DevClienteViewModel) {
+  fun findLoja(storeno: Int): Loja? {
+    val lojas = Loja.allLojas()
+    return lojas.firstOrNull { it.no == storeno }
+  }
+
+  fun findAllLojas(): List<Loja> {
+    return Loja.allLojas()
+  }
+
+  fun updateView() {
+    val filtro = subView.filtro()
+    val produtos = EntradaDevCliProList.findAll(filtro).distinctBy {dev ->
+      "${dev.loja} ${dev.prdno} ${dev.grade} ${dev.tipoPrd}"
+    }
+    subView.updateProdutos(produtos)
+  }
+
+  fun imprimeProdutos() = viewModel.exec {
+    val produtos = subView.produtosSelecionados()
+    if (produtos.isEmpty()) {
+      fail("Não há produtos selecionados")
+    }
+
+    val countEntregador = produtos.map { it.userEntregaNo ?: 0 }.distinct().size
+    if (countEntregador != 1) {
+      fail("Possui mais de um entregador")
+    }
+
+    val countRecebedor = produtos.map { it.userRecebimentoNo ?: 0 }.distinct().size
+    if (countRecebedor != 1) {
+      fail("Possui mais de um recebedor")
+    }
+
+    val countTipo = produtos.map { it.produtoTipoP }.distinct().size
+    if (countTipo != 1) {
+      fail("Foi seleciona produtos de mais de um tipo")
+    }
+
+    val relatorio = ProdutosDevolucao("Devolucoes de Clientes com Produtos")
+    relatorio.print(produtos.sortedBy { it.ni }, subView.printerPreview(loja = 0))
+  }
+
+  val subView
+    get() = viewModel.view.tabDevCliProduto
+}
+
+interface ITabDevCliProduto : ITabView {
+  fun filtro(): FiltroEntradaDevCliProList
+  fun updateProdutos(produtos: List<EntradaDevCliProList>)
+  fun produtosSelecionados(): List<EntradaDevCliProList>
+  fun reloadGrid()
+}
