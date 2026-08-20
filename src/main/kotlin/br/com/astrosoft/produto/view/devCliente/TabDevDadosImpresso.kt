49ee7ab0d5e4896da1e731549ddc8e77fcadeed3
Ivaney Vieira de Sales <ivaney@pintos.com.br>
2026-08-20 16:08:35 -0300

    Copia painel Credito impressors

---
+++ src/main/kotlin/br/com/astrosoft/produto/view/devCliente/TabDevDadosImpresso.kt
@@ -0,0 +1,221 @@
+package br.com.astrosoft.produto.view.devCliente
+
+import br.com.astrosoft.framework.model.config.AppConfig
+import br.com.astrosoft.framework.view.vaadin.TabPanelGrid
+import br.com.astrosoft.framework.view.vaadin.helper.DialogHelper
+import br.com.astrosoft.framework.view.vaadin.helper.addColumnButton
+import br.com.astrosoft.framework.view.vaadin.helper.columnGrid
+import br.com.astrosoft.framework.view.vaadin.helper.localePtBr
+import br.com.astrosoft.produto.model.beans.*
+import br.com.astrosoft.produto.viewmodel.devCliente.ITabDevDados
+import br.com.astrosoft.produto.viewmodel.devCliente.TabDevDadosViewModel
+import com.github.mvysny.karibudsl.v10.datePicker
+import com.github.mvysny.karibudsl.v10.select
+import com.github.mvysny.karibudsl.v10.textField
+import com.vaadin.flow.component.datepicker.DatePicker
+import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.select.Select
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.value.ValueChangeMode
+import java.time.LocalDate
+
+class TabDevDadosImpresso(val viewModel: TabDevDadosViewModel) :
+  TabPanelGrid<DadosDev>(DadosDev::class), ITabDevDados {
+  private lateinit var cmbLoja: Select<Loja>
+  private lateinit var edtPesquisa: TextField
+  private lateinit var edtDataInicial: DatePicker
+  private lateinit var edtDataFinal: DatePicker
+  private var dlgProduto: DlgProdutosDadosDev? = null
+
+  fun init() {
+    cmbLoja.setItems(viewModel.findAllLojas() + listOf(Loja.lojaZero))
+    val user = AppConfig.userLogin() as? UserSaci
+    cmbLoja.isReadOnly = user?.lojaVale != 0
+    cmbLoja.value = viewModel.findLoja(user?.lojaVale ?: 0) ?: Loja.lojaZero
+  }
+
+  override fun printerUser(): List<String> {
+    val username = AppConfig.userLogin() as? UserSaci
+    return username?.impressoraDev.orEmpty().toList()
+  }
+
+  override fun HorizontalLayout.toolBarConfig() {
+    cmbLoja = select("Loja") {
+      this.setItemLabelGenerator { item ->
+        item.descricao
+      }
+      addValueChangeListener {
+        if (it.isFromClient)
+          viewModel.updateView()
+      }
+    }
+    init()
+    edtPesquisa = textField("Pesquisa") {
+      this.width = "300px"
+      valueChangeMode = ValueChangeMode.TIMEOUT
+      addValueChangeListener {
+        viewModel.updateView()
+      }
+    }
+    edtDataInicial = datePicker("Data inicial") {
+      this.localePtBr()
+      this.value = LocalDate.now()
+
+      addValueChangeListener {
+        viewModel.updateView()
+      }
+    }
+    edtDataFinal = datePicker("Data Final") {
+      this.localePtBr()
+      this.value = LocalDate.now()
+
+      addValueChangeListener {
+        viewModel.updateView()
+      }
+    }
+  }
+
+  override fun Grid<DadosDev>.gridPanel() {
+    this.addClassName("styling")
+
+    columnGrid(DadosDev::loja, header = "Loja")
+
+    addColumnButton(iconButton = VaadinIcon.PRINT, tooltip = "Imprimir vale troca", header = "Imprimir") { nota ->
+      imprimeVale(nota)
+    }
+
+    addColumnButton(VaadinIcon.FILE_TABLE, "Produtos", "Produtos") { nota ->
+      dlgProduto = DlgProdutosDadosDev(viewModel, nota)
+      dlgProduto?.showDialog {
+        viewModel.updateView()
+      }
+    }
+
+    val user = AppConfig.userLogin() as? UserSaci
+
+    addColumnButton(
+      iconButton = VaadinIcon.SIGN_IN,
+      tooltip = "Autoriza Solicitação",
+      header = "Solicitação",
+      configIcon = { icon, nota ->
+        if (nota.tipoDevEnum != null && nota.produtoTrocaEnum != null) {
+          icon.color = "yellow"
+        }
+      }
+    ) { nota ->
+      execSolicitacoes(nota)
+    }
+
+    if (user?.defazSolicitacao == true) {
+      addColumnButton(VaadinIcon.TRASH, "Desfazer Solicitação", "Desfaz") { nota ->
+        execDesfazSolicitacoes(nota)
+      }
+    }
+
+    columnGrid(DadosDev::loginSolicitacao, header = "Autorização")
+    columnGrid(DadosDev::loginTroca, header = "Assina Troca")
+
+    columnGrid(DadosDev::ni, header = "NI")
+    columnGrid(DadosDev::nfDevolucao, header = "NF Dev")
+    columnGrid(DadosDev::dataDevolucao, header = "Data", width = null)
+    columnGrid(DadosDev::valorDev, header = "Valor Dev")
+    columnGrid(DadosDev::descTipo, header = "Tipo do Crédito") {
+      this.setPartNameGenerator() { nota ->
+        if ((nota.custnoObs ?: 0) == 0) {
+          null
+        } else
+          if (nota.nomeClienteObs.isNullOrBlank()) {
+            "vermelho"
+          } else {
+            null
+          }
+      }
+    }
+    columnGrid(DadosDev::nfVenda, header = "NFVenda")
+    columnGrid(DadosDev::dataVenda, header = "Data", width = null)
+    columnGrid(DadosDev::custnoVend, header = "Cliente")
+    columnGrid(DadosDev::codCliente, header = "For")
+    columnGrid(DadosDev::nomeCliente, header = "Nome")
+  }
+
+  private fun imprimeVale(nota: DadosDev) {
+    val assinatura = nota.loginTroca ?: ""
+
+    if (assinatura.isBlank()) {
+      DialogHelper.showWarning("Devolução sem Assinatura de Troca.")
+      return
+    }
+
+    if (nota.loginSolicitacao == null) {
+      val formAutoriza = FormAutoriza()
+      DialogHelper.showForm(caption = "Autoriza Impressão", form = formAutoriza) {
+        viewModel.imprimeValeTroca(nota, formAutoriza.login, formAutoriza.senha)
+        viewModel.updateView()
+      }
+    } else {
+      viewModel.imprimeValeTroca(nota)
+      viewModel.updateView()
+    }
+  }
+
+  private fun execDesfazSolicitacoes(nota: DadosDev) {
+    if (nota.tipoDevEnum == null && nota.produtoTrocaEnum == null && nota.loginSolicitacao == null && nota.loginTroca == null) {
+      DialogHelper.showError("Não existe solicitação para desfazer")
+    } else {
+      DialogHelper.showQuestion("Desfaz a solicitação?") {
+        viewModel.desfazSolicitacao(nota)
+      }
+    }
+  }
+
+  private fun execSolicitacoes(nota: DadosDev) {
+    val form = FormSolicitacaoDevDados(nota)
+
+    DialogHelper.showForm(caption = "Autoriza Devolução", form = form) {
+      val result = form.validaFiltro()
+      result.onFailure {
+        DialogHelper.showWarning(it.message ?: "Erro no filtro")
+      }
+      result.onSuccess { solicitacaoTroca ->
+        val solicitacaoTroca: SolicitacaoTrocaSimples = solicitacaoTroca
+        viewModel.autorizaSolicitacao(nota, solicitacaoTroca)
+      }
+    }
+  }
+
+  override fun filtro(): FiltroDadosDev {
+    return FiltroDadosDev(
+      loja = cmbLoja.value?.no ?: 0,
+      pesquisa = edtPesquisa.value ?: "",
+      dataInicial = edtDataInicial.value,
+      dataFinal = edtDataFinal.value,
+      impresso = false
+    )
+  }
+
+  override fun updateNotas(notas: List<DadosDev>) {
+    updateGrid(notas)
+  }
+
+  override fun updateProdutos() {
+    dlgProduto?.update()
+  }
+
+  override fun fechaFormProduto() {
+    dlgProduto?.fecha()
+  }
+
+  override fun isAuthorized(): Boolean {
+    val username = AppConfig.userLogin() as? UserSaci
+    return username?.devDados == true
+  }
+
+  override val label: String
+    get() = "Imp Crédito"
+
+  override fun updateComponent() {
+    viewModel.updateView()
+  }
+}
---
+++ src/main/kotlin/br/com/astrosoft/produto/viewmodel/devCliente/TabDevDadosImpressoViewModel.kt
@@ -0,0 +1,269 @@
+package br.com.astrosoft.produto.viewmodel.devCliente
+
+import br.com.astrosoft.framework.model.config.AppConfig
+import br.com.astrosoft.framework.viewmodel.ITabView
+import br.com.astrosoft.framework.viewmodel.fail
+import br.com.astrosoft.produto.model.beans.*
+import br.com.astrosoft.produto.model.printText.ValeTrocaDadosDev
+
+class TabDevDadosViewModel(val viewModel: DevClienteViewModel) {
+  fun findLoja(storeno: Int): Loja? {
+    val lojas = Loja.allLojas()
+    return lojas.firstOrNull { it.no == storeno }
+  }
+
+  fun findAllLojas(): List<Loja> {
+    return Loja.allLojas()
+  }
+
+  fun updateView() = viewModel.exec {
+    val filtro = subView.filtro()
+    val notas = DadosDev.findAll(filtro)
+    subView.updateNotas(notas)
+  }
+
+  fun ajusteProduto(ajuste: AjusteProduto) {
+    val produto = ajuste.produto
+    produto.marcaAjuste(ajuste)
+  }
+
+  fun desfazSolicitacao(nota: DadosDev) {
+    nota.apagaDados()
+    updateView()
+  }
+
+  fun autorizaSolicitacao(nota: DadosDev, solicitacaoTroca: SolicitacaoTrocaSimples) = viewModel.exec {
+    nota.tipoDevEnum = solicitacaoTroca.solicitacaoTrocaEnnum
+    nota.produtoTrocaEnum = solicitacaoTroca.produtoTrocaEnum
+    nota.userSolicitacao = null
+    nota.update()
+
+    updateView()
+  }
+
+  fun salvaNfEntRet(nota: DadosDev, nfNumero: Int) = viewModel.exec {
+    nota.nfEntRet = nfNumero
+    nota.salvaNfEntRet()
+  }
+
+  fun validaProcesamento(user: UserSaci?, nota: DadosDev, produtos: List<DadosDevProduto>): Boolean {
+    try {
+      val assina = nota.loginTroca
+      if (assina != null) {
+        fail("Troca já Assinada")
+      }
+
+      user ?: fail("Usuário inválido")
+      produtos.ifEmpty {
+        fail("Nenhum produto selecionado")
+      }
+
+      val solicitacao = nota.tipoDevEnum ?: fail("Tipo de devolução não informada")
+      val produto = nota.produtoTrocaEnum ?: fail("Tipo de devolução (com ou sem produto) não informada")
+
+      val produtosDevComProduto = produtos.sumOf { it.quantidadeCom ?: 0 }
+      val produtosDevSemProduto = produtos.sumOf { it.quantidadeSem ?: 0 }
+
+      val tipoResultante = when {
+        produtosDevComProduto > 0 && produtosDevSemProduto == 0 -> EProdutoTroca.Com
+        produtosDevComProduto == 0 && produtosDevSemProduto > 0 -> EProdutoTroca.Sem
+        produtosDevComProduto > 0 && produtosDevSemProduto > 0  -> EProdutoTroca.Misto
+        else                                                    -> fail("Não há produtos para devolução")
+      }
+
+      if (tipoResultante != produto) {
+        fail("Divergência: No filtro marcado ${produto.descricao} e na linha do produto marcado como ${tipoResultante.descricao}")
+      }
+      /*********************************************************************************/
+
+      produtos.forEach { prd ->
+        val qtdCom = prd.quantidadeCom ?: 0
+        val qtdSem = prd.quantidadeSem ?: 0
+        val quantDev = prd.quantidadeDev ?: 0
+        if (quantDev != (qtdSem + qtdCom)) {
+          fail("Quantidade devolvida diferente da autorizada")
+        }
+      }
+
+      val valorProdutos = produtos.sumOf { prd ->
+        prd.valorTotal
+      }
+      val valorDevolucao = user.valorDevolucao
+
+      when (solicitacao) {
+        ESolicitacaoTroca.Troca       -> {
+
+          if (produto == EProdutoTroca.Com) {
+            if (valorProdutos > valorDevolucao) {
+              fail("Valor da devolução maior que o autorizado")
+            }
+          } else {
+            if (valorProdutos > valorDevolucao) {
+              fail("Valor da devolução maior que o autorizado")
+            }
+          }
+        }
+
+        ESolicitacaoTroca.Estorno     -> {
+          if (valorProdutos > valorDevolucao) {
+            fail("Valor da devolução maior que o autorizado")
+          }
+        }
+
+        ESolicitacaoTroca.Reembolso   -> {
+          if (valorProdutos > valorDevolucao) {
+            fail("Valor da devolução maior que o autorizado")
+          }
+        }
+
+        ESolicitacaoTroca.MudaCliente -> {
+          if (valorProdutos > valorDevolucao) {
+            fail("Valor da devolução maior que o autorizado")
+          }
+        }
+      }
+
+      /***********************************************************************************/
+
+    } catch (e: Exception) {
+      val msg = e.message
+      viewModel.view.showWarning(msg ?: "Erro genérico")
+      return false
+    }
+    return true
+  }
+
+  fun autorizaNotaVenda(nota: DadosDev, produtos: List<DadosDevProduto>, login: String, senha: String) =
+      viewModel.exec {
+        nota.tipoDevEnum ?: fail("Nota sem solicitação de troca")
+        nota.produtoTrocaEnum ?: fail("Nota sem produto de troca")
+
+        val user = UserSaci.userLogin(login, senha)
+
+        if (!validaProcesamento(user, nota, produtos)) {
+          return@exec
+        }
+
+        user ?: fail("Usuário inválido")
+
+        if (!user.autorizaDev) {
+          fail("Usuário sem permissão para autorizar devolução")
+        }
+
+        nota.userTroca = user.no
+        nota.update()
+        produtos.forEach { prd ->
+          prd.update()
+        }
+        subView.fechaFormProduto()
+        subView.updateProdutos()
+        updateView()
+      }
+
+  fun desautorizaTroca(nota: DadosDev, produto: DadosDevProduto) = viewModel.exec {
+    viewModel.view.showQuestion("Confirma desautorizar devolução do produto ${produto.codigo}?") {
+      viewModel.exec {
+        val user = AppConfig.userLogin() as? UserSaci
+
+        if (user?.desautorizaDev == false) {
+          fail("Usuário sem permissão")
+        }
+
+        if ((produto.ni ?: 0) != 0) {
+          fail("A nota de devolução já foi emitida")
+        }
+
+        produto.deleteDados()
+
+        subView.updateProdutos()
+      }
+    }
+  }
+
+  /**************************** imprimeValeTroca ************************************/
+
+  fun imprimeValeTroca(nota: DadosDev) = viewModel.exec {
+    val userno = nota.userSolicitacao ?: fail("Usuário ou senha inválidos")
+    val user = UserSaci.findUser(userno) ?: fail("Usuário ou senha inválidos")
+    imprimeValeTroca(nota, user)
+  }
+
+  fun imprimeValeTroca(nota: DadosDev, login: String, senha: String) = viewModel.exec {
+    val user = UserSaci.userLogin(login, senha)
+    user ?: fail("Usuário ou senha inválidos")
+
+    imprimeValeTroca(nota, user)
+  }
+
+  private fun imprimeValeTroca(
+    nota: DadosDev,
+    user: UserSaci
+  ) {
+    val solicitacaoTrocaEnum = nota.tipoDevEnum ?: fail("Tipo de Crédito não Informado")
+    val produtoTrocaEnum = nota.produtoTrocaEnum ?: fail("Tipo da Devolução não informada")
+    val login = user.login
+
+    when (solicitacaoTrocaEnum) {
+      ESolicitacaoTroca.Troca       -> when (produtoTrocaEnum) {
+        EProdutoTroca.Com   -> if (!user.autorizaImpTrocaP) {
+          fail("Impressão de Troca com produto não autorizada para usuário $login")
+        }
+
+        EProdutoTroca.Sem   -> if (!user.autorizaImpTroca) {
+          fail("Impressão de Troca sem produto não autorizada para usuário $login")
+        }
+
+        EProdutoTroca.Misto -> if (!user.autorizaImpTrocaP || !user.autorizaImpTroca) {
+          fail("Impressão de Troca mista de produto não autorizada para usuário $login")
+        }
+      }
+
+      ESolicitacaoTroca.Estorno     -> if (!user.autorizaImpEstorno) {
+        fail("Impressão de Estorno de produto não autorizado para usuário $login")
+      }
+
+      ESolicitacaoTroca.Reembolso   -> if (!user.autorizaImpReembolso) {
+        fail("Impressão de Reembolso de produto não autorizado para usuário $login")
+      }
+
+      ESolicitacaoTroca.MudaCliente -> if (!user.autorizaImpMuda) {
+        fail("Impressão de Mudança de cliente não autorizada para usuário $login")
+      }
+    }
+
+    nota.tipoDevEnum = solicitacaoTrocaEnum
+    nota.produtoTrocaEnum = produtoTrocaEnum
+    nota.userSolicitacao = user.no
+    nota.loginSolicitacao = user.login
+    nota.nomeSolicitacao = user.name
+    nota.update()
+
+    imprime(nota)
+  }
+
+  private fun imprime(nota: DadosDev) {
+    val relatorio = ValeTrocaDadosDev(nota)
+
+    val dados = nota.produtos
+    val printer = subView.printerPreview(loja = 0) { impressora ->
+      if(nota.impressora.isBlank()) {
+        nota.marcaImpresso(Impressora(no = 0, name = impressora))
+        updateView()
+      }
+    }
+
+    relatorio.print(dados = dados, printer = printer)
+  }
+
+  /**************************** imprimeValeTroca ************************************/
+
+  val subView
+    get() = viewModel.view.tabDevDados
+}
+
+interface ITabDevDados : ITabView {
+  fun filtro(): FiltroDadosDev
+  fun updateNotas(notas: List<DadosDev>)
+  fun updateProdutos()
+  fun fechaFormProduto()
+}
