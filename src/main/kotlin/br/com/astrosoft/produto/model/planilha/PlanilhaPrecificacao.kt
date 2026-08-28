f5b498d18be06926bf00e0f74eeb4aa6f08f47f7
Ivaney Vieira de Sales <ivaney@pintos.com.br>
2026-08-28 14:11:28 -0300

    Atualização da planilha de precificação

--- src/main/kotlin/br/com/astrosoft/produto/model/planilha/PlanilhaPrecificacao.kt
+++ src/main/kotlin/br/com/astrosoft/produto/model/planilha/PlanilhaPrecificacao.kt
@@ -5,38 +5,44 @@ import br.com.astrosoft.produto.model.beans.Precificacao
 
 class PlanilhaPrecificacao : Planilha<Precificacao>("Precificação") {
   init {
-    columnSheet(header = "Cod", property = Precificacao::codigo)
-    columnSheet(header = "Descrição", property = Precificacao::descricao)
-    columnSheet(header = "Cod For", property = Precificacao::vendno)
-    columnSheet(header = "Tipo", property = Precificacao::typeno)
-    columnSheet(header = "CL", property = Precificacao::clno)
-    columnSheet(header = "NCM", property = Precificacao::ncm)
-    columnSheet(header = "Rotulo", property = Precificacao::rotulo)
-    columnSheet(header = "Trib", property = Precificacao::tributacao)
-    columnSheet(header = "MVA", property = Precificacao::mvap)
-    columnSheet(header = "ICMS Ent", property = Precificacao::creditoICMS)
-    columnSheet(header = "P. Fab", property = Precificacao::pcfabrica)
-    columnSheet(header = "IPI", property = Precificacao::ipi)
-    columnSheet(header = "Emb", property = Precificacao::embalagem)
-    columnSheet(header = "IR ST", property = Precificacao::retido)
-    columnSheet(header = "C. ICMS", property = Precificacao::icmsp)
-    columnSheet(header = "Frete", property = Precificacao::frete)
-    columnSheet(header = "ICMS Calc F", property = Precificacao::freteICMSCalc)
-    columnSheet(header = "ICMS F", property = Precificacao::freteICMS)
-    columnSheet(header = "C.Cont", property = Precificacao::custoContabil)
-    columnSheet(header = "P.Custo", property = Precificacao::precoCusto)
-    columnSheet(header = "Dif", property = Precificacao::diferencaCusto)
-    columnSheet(header = "ICM Sai", property = Precificacao::icms)
-    columnSheet(header = "FCP", property = Precificacao::fcp)
-    columnSheet(header = "Pis", property = Precificacao::pis)
-    columnSheet(header = "IR", property = Precificacao::ir)
-    columnSheet(header = "CS", property = Precificacao::contrib)
-    columnSheet(header = "CPMF", property = Precificacao::cpmf)
-    columnSheet(header = "Desp", property = Precificacao::fixa)
-    columnSheet(header = "Out", property = Precificacao::outras)
-    columnSheet(header = "Lucro", property = Precificacao::lucroLiq)
-    columnSheet(header = "P.Sug.", property = Precificacao::precoSug)
-    columnSheet(header = "P.Ref.", property = Precificacao::precoRef)
+    columnSheet(property = Precificacao::codigo, header = "Cod")
+    columnSheet(Precificacao::descricao, "Descrição")
+
+    columnSheet(Precificacao::estoque, "Est")
+
+    columnSheet(Precificacao::nfValor, "V. NF")
+    columnSheet(Precificacao::pcfabrica, "P. Fab")
+
+    columnSheet(Precificacao::nfIpi, "IPI NF")
+    columnSheet(Precificacao::ipi, "IPI")
+
+    columnSheet(Precificacao::nfIrst, "IR ST NF")
+    columnSheet(Precificacao::retido, "IR ST")
+
+    columnSheet(Precificacao::nfIcms, "ICMS NF")
+    columnSheet(Precificacao::icmsp, "C. ICMS")
+
+    columnSheet(Precificacao::nfFrete, "Frete NF")
+    columnSheet(Precificacao::frete, "Frete")
+
+    columnSheet(Precificacao::pisCofins, "Pis/Cofins")
+    columnSheet(Precificacao::custoContabil, "C.Cont")
+    columnSheet(Precificacao::embalagem, "Emb")
+    columnSheet(Precificacao::vendno, "Cod For")
+    columnSheet(Precificacao::typeno, "Tipo")
+    columnSheet(Precificacao::clno, "CL")
+    columnSheet(Precificacao::ncm, "NCM")
+    columnSheet(Precificacao::rotulo, "Rótulo")
+    columnSheet(Precificacao::tributacao, "Trib")
+    columnSheet(Precificacao::mvap, "MVA")
+    columnSheet(Precificacao::creditoICMS, "ICMS Ent")
+    columnSheet(Precificacao::freteICMSCalc, "ICMS Calc F")
+    columnSheet(Precificacao::freteICMS, "ICMS F")
+
+    columnSheet(Precificacao::cfinanceiro, "C. Fin")
+    columnSheet(Precificacao::precoCusto, "P.Custo")
+    columnSheet(Precificacao::diferencaCusto, "Dif")
+
   }
 }
 
